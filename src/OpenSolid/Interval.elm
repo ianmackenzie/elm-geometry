8323b74e400337f97efddabc41ac3f1a6fe511fd
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2016-01-12 15:48:23 +1100

    Add Interval.contains, Interval.overlaps

--- src/OpenSolid/Interval.elm
+++ src/OpenSolid/Interval.elm
@@ -4,6 +4,8 @@ module OpenSolid.Interval
   , width
   , interpolated
   , median
+  , contains
+  , overlaps
   ) where
 
 
@@ -31,3 +33,13 @@ interpolated parameter interval =
 median: Interval -> Float
 median =
   interpolated 0.5
+
+
+contains: Float -> Interval -> Bool
+contains value interval =
+  interval.lowerBound <= value && value <= interval.upperBound
+
+
+overlaps: Interval -> Interval -> Bool
+overlaps other interval =
+  interval.lowerBound <= other.upperBound && interval.upperBound >= other.lowerBound
