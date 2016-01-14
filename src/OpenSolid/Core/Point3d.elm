5195b8658c733727b2b7bf0148bd4ae58d89516a
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2016-01-14 18:13:33 +1100

    Add Point3d

---
+++ src/OpenSolid/Core/Point3d.elm
@@ -0,0 +1,48 @@
+module OpenSolid.Core.Point3d
+  ( origin
+  , components
+  , squaredDistanceTo
+  , distanceTo
+  , transformedBy
+  , plus
+  , minus
+  ) where
+
+
+import OpenSolid.Core exposing (Point3d, Vector3d, Transformation3d)
+import OpenSolid.Core.Vector3d as Vector3d
+
+
+origin: Point3d
+origin =
+  Point3d 0 0 0
+
+
+components: Point3d -> (Float, Float, Float)
+components point =
+  (point.x, point.y, point.z)
+
+
+squaredDistanceTo: Point3d -> Point3d -> Float
+squaredDistanceTo other =
+  minus other >> Vector3d.squaredLength
+
+
+distanceTo: Point3d -> Point3d -> Float
+distanceTo other =
+  minus other >> Vector3d.length
+
+
+transformedBy: Transformation3d -> Point3d -> Point3d
+transformedBy =
+  .ofPoint
+
+
+plus: Vector3d -> Point3d -> Point3d
+plus vector point =
+  Point3d (point.x + vector.x) (point.y + vector.y)
+
+
+minus: Point3d -> Point3d -> Vector3d
+minus other point =
+  Vector3d (point.x - other.x) (point.y - other.y)
