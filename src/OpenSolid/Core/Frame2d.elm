d6738d9db321e30e55ff1ac8e989a3856fff793f
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2016-01-17 15:30:57 +1100

    Add Frame2d module

---
+++ src/OpenSolid/Core/Frame2d.elm
@@ -0,0 +1,37 @@
+module OpenSolid.Core.Frame2d where
+  ( global
+  , xAxis
+  , yAxis
+  , transformedBy
+  ) where
+
+
+import OpenSolid.Core exposing (Point2d, Direction2d, Axis2d, Transformation2d)
+import OpenSolid.Core.Point2d as Point2d
+import OpenSolid.Core.Direction2d as Direction2d
+
+
+global: Frame2d
+global =
+  Frame2d Point2d.origin Direction2d.x Direction2d.y
+
+
+xAxis: Frame2d -> Axis2d
+xAxis frame =
+  Axis2d frame.originPoint frame.xDirection
+
+
+yAxis: Frame2d -> Axis2d
+yAxis frame =
+  Axis2d frame.originPoint frame.yDirection
+
+
+transformedBy: Transformation2d -> Frame2d -> Frame2d
+transformedBy transformation frame =
+  let
+    originPoint = Point2d.transformedBy transformation frame.originPoint
+    transformedDirection = Direction2d.transformedBy transformation
+    xDirection = transformedDirection frame.xDirection
+    yDirection = transformedDirection frame.yDirection
+  in
+    Frame2d originPoint xDirection yDirection
