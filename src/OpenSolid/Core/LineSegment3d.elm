6457dbf04878b35cb3447b3e3dc7f7dcc3b7cf65
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2016-01-18 14:40:59 +1100

    Add LineSegment3d module

---
+++ src/OpenSolid/Core/LineSegment3d.elm
@@ -0,0 +1,54 @@
+module OpenSolid.Core.LineSegment3d
+  ( endpoints
+  , vector
+  , direction
+  , normalDirection
+  , squaredLength
+  , length
+  , transformedBy
+  ) where
+
+
+import OpenSolid.Core exposing (..)
+import OpenSolid.Core.Vector3d as Vector3d
+import OpenSolid.Core.Point3d as Point3d
+
+
+endpoints: LineSegment3d -> (Point3d, Point3d)
+endpoints lineSegment =
+  (lineSegment.firstEndpoint, lineSegment.secondEndpoint)
+
+
+vector: LineSegment3d -> Vector3d
+vector lineSegment =
+  Point3d.minus lineSegment.firstEndpoint lineSegment.secondEndpoint
+
+
+direction: LineSegment3d -> Direction3d
+direction =
+  vector >> Vector3d.direction
+
+
+normalDirection: LineSegment3d -> Direction3d
+normalDirection =
+  vector >> Vector3d.normalDirection
+
+
+squaredLength: LineSegment3d -> Float
+squaredLength =
+  vector >> Vector3d.squaredLength
+
+
+length: LineSegment3d -> Float
+length =
+  vector >> Vector3d.length
+
+
+transformedBy: Transformation3d -> LineSegment3d -> LineSegment3d
+transformedBy transformation lineSegment =
+  let
+    transformPoint = Point3d.transformedBy transformation
+    firstEndpoint = transformPoint lineSegment.firstEndpoint
+    secondEndpoint = transformPoint lineSegment.secondEndpoint
+  in
+    LineSegment3d firstEndpoint secondEndpoint
