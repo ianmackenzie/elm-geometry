c06d4a1855b622e13a93598c81acceae3b652fb8
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2016-01-19 14:51:09 +1100

    Temporarily disable out of date modules

--- examples/test.elm
+++ examples/test.elm
@@ -4,25 +4,25 @@ import String
 import Signal exposing (Signal)
 import Debug
 import OpenSolid.Core exposing (..)
-import OpenSolid.Core.Axis2d as Axis2d
-import OpenSolid.Core.Axis3d as Axis3d
-import OpenSolid.Core.Box2d as Box2d
-import OpenSolid.Core.Box3d as Box3d
+--import OpenSolid.Core.Axis2d as Axis2d
+--import OpenSolid.Core.Axis3d as Axis3d
+--import OpenSolid.Core.Box2d as Box2d
+--import OpenSolid.Core.Box3d as Box3d
 import OpenSolid.Core.Direction2d as Direction2d
-import OpenSolid.Core.Direction3d as Direction3d
-import OpenSolid.Core.Frame2d as Frame2d
-import OpenSolid.Core.Frame3d as Frame3d
+--import OpenSolid.Core.Direction3d as Direction3d
+--import OpenSolid.Core.Frame2d as Frame2d
+--import OpenSolid.Core.Frame3d as Frame3d
 import OpenSolid.Core.Interval as Interval
-import OpenSolid.Core.LineSegment2d as LineSegment2d
-import OpenSolid.Core.Plane3d as Plane3d
-import OpenSolid.Core.Point2d as Point2d
-import OpenSolid.Core.Point3d as Point3d
-import OpenSolid.Core.Scalar as Scalar
-import OpenSolid.Core.Transformation2d as Transformation2d
-import OpenSolid.Core.Transformation3d as Transformation3d
-import OpenSolid.Core.Triangle2d as Triangle2d
+--import OpenSolid.Core.LineSegment2d as LineSegment2d
+--import OpenSolid.Core.Plane3d as Plane3d
+--import OpenSolid.Core.Point2d as Point2d
+--import OpenSolid.Core.Point3d as Point3d
+--import OpenSolid.Core.Scalar as Scalar
+--import OpenSolid.Core.Transformation2d as Transformation2d
+--import OpenSolid.Core.Transformation3d as Transformation3d
+--import OpenSolid.Core.Triangle2d as Triangle2d
 import OpenSolid.Core.Vector2d as Vector2d
-import OpenSolid.Core.Vector3d as Vector3d
+--import OpenSolid.Core.Vector3d as Vector3d
 
 
 type alias State =
@@ -73,38 +73,38 @@ view state =
   let
     intervalWidth = Interval.width (Interval 2 3)
     vectorLength = Vector2d.length (Vector2d 1 1)
-    pointDifference = Point2d.minus Point2d.origin (Point2d 1 2)
-    rotation = Transformation2d.rotationAbout Point2d.origin (degrees 45)
-    lineSegment = LineSegment2d (Point2d 1 0) (Point2d 2 0)
-    transformedSegment = LineSegment2d.transformedBy rotation lineSegment
-    mixedDotProduct = Vector2d.dot Direction2d.x (Vector2d 2 3)
-    rotatedDirection = Direction2d.transformedBy rotation Direction2d.x
-    angledDotProduct = Vector2d.dot rotatedDirection (Vector2d 2 3)
-    triangle = Triangle2d Point2d.origin (Point2d 1 0) (Point2d 0 1)
-    triangleArea = Triangle2d.area triangle
-    contains1 = Triangle2d.contains (Point2d 0.5 0.5) triangle
-    contains2 = Triangle2d.contains (Point2d 1 1) triangle
-    placedTriangle = Triangle2d.placedOntoPlane Plane3d.xz triangle
-    axis = Axis2d Point2d.origin (Direction2d.polar (degrees -45))
-    projectPoint = Point2d.projectedOntoAxis axis
-    list3 a b c = [a, b, c]
-    projectedPoints = Triangle2d.mapReduce projectPoint list3 triangle
+    --pointDifference = Point2d.minus Point2d.origin (Point2d 1 2)
+    --rotation = Transformation2d.rotationAbout Point2d.origin (degrees 45)
+    --lineSegment = LineSegment2d (Point2d 1 0) (Point2d 2 0)
+    --transformedSegment = LineSegment2d.transformedBy rotation lineSegment
+    --mixedDotProduct = Vector2d.dot Direction2d.x (Vector2d 2 3)
+    --rotatedDirection = Direction2d.transformedBy rotation Direction2d.x
+    --angledDotProduct = Vector2d.dot rotatedDirection (Vector2d 2 3)
+    --triangle = Triangle2d Point2d.origin (Point2d 1 0) (Point2d 0 1)
+    --triangleArea = Triangle2d.area triangle
+    --contains1 = Triangle2d.contains (Point2d 0.5 0.5) triangle
+    --contains2 = Triangle2d.contains (Point2d 1 1) triangle
+    --placedTriangle = Triangle2d.placedOntoPlane Plane3d.xz triangle
+    --axis = Axis2d Point2d.origin (Direction2d.polar (degrees -45))
+    --projectPoint = Point2d.projectedOntoAxis axis
+    --list3 a b c = [a, b, c]
+    --projectedPoints = Triangle2d.mapReduce projectPoint list3 triangle
   in
     Html.div []
       [ line "Interval width" intervalWidth
       , line "Vector length" vectorLength
-      , line "Point difference" pointDifference
-      , line "Transformed line segment" transformedSegment
-      , line "Mixed dot product" mixedDotProduct
-      , line "Rotated direction" rotatedDirection
-      , line "Angled dot product" angledDotProduct
+      --, line "Point difference" pointDifference
+      --, line "Transformed line segment" transformedSegment
+      --, line "Mixed dot product" mixedDotProduct
+      --, line "Rotated direction" rotatedDirection
+      --, line "Angled dot product" angledDotProduct
       , line "Current time" (timeString state.currentTime)
-      , line "Triangle area" triangleArea
-      , line "Contains 1" contains1
-      , line "Contains 2" contains2
-      , line "Placed triangle" placedTriangle
-      , line "Axis" axis
-      , line "Projected points" projectedPoints
+      --, line "Triangle area" triangleArea
+      --, line "Contains 1" contains1
+      --, line "Contains 2" contains2
+      --, line "Placed triangle" placedTriangle
+      --, line "Axis" axis
+      --, line "Projected points" projectedPoints
       ]
 
 
