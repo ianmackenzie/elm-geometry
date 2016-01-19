0080d6b33789ab4347632e6e3bd0149de582ca3a
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2016-01-19 14:48:47 +1100

    Begin switching to union types
    Possible now that all types are defined in Core.elm and can be shared
    between modules; has better type safety than records and allows more
    convenient destructuring (destructured names don't have to match field
    names)

--- src/OpenSolid/Core.elm
+++ src/OpenSolid/Core.elm
@@ -1,168 +1,117 @@
 module OpenSolid.Core
-  ( Vector2d
-  , Vector3d
-  , Point2d
-  , Point3d
-  , Direction2d
-  , Direction3d
-  , Axis2d
-  , Axis3d
-  , Plane3d
-  , Frame2d
-  , Frame3d
-  , Transformation2d
-  , Transformation3d
-  , LineSegment2d
-  , LineSegment3d
-  , Triangle2d
-  , Triangle3d
-  , Interval
-  , Box2d
-  , Box3d
+  ( Vector2d (Vector2d)
+  , Vector3d (Vector3d)
+  , Point2d (Point2d)
+  , Point3d (Point3d)
+  , Direction2d (Direction2d)
+  , Direction3d (Direction3d)
+  , Axis2d (Axis2d)
+  , Axis3d (Axis3d)
+  , Plane3d (Plane3d)
+  , Frame2d (Frame2d)
+  , Frame3d (Frame3d)
+  , Transformation2d (Transformation2d)
+  , Transformation3d (Transformation3d)
+  , LineSegment2d (LineSegment2d)
+  , LineSegment3d (LineSegment3d)
+  , Triangle2d (Triangle2d)
+  , Triangle3d (Triangle3d)
+  , Interval (Interval)
+  , Box2d (Box2d)
+  , Box3d (Box3d)
   ) where
 
 
 -- Primitives
 
 
-type alias Vector2d =
-  { x: Float
-  , y: Float
-  }
+type Vector2d
+  = Vector2d Float Float
 
 
-type alias Vector3d =
-  { x: Float
-  , y: Float
-  , z: Float
-  }
+type Vector3d
+  = Vector3d Float Float Float
 
 
-type alias Point2d =
-  { x: Float
-  , y: Float
-  }
+type Point2d
+  = Point2d Float Float
 
 
-type alias Point3d =
-  { x: Float
-  , y: Float
-  , z: Float
-  }
+type Point3d
+  = Point3d Float Float Float
 
 
-type alias Direction2d =
-  { x: Float
-  , y: Float
-  }
+type Direction2d
+  = Direction2d Vector2d
 
 
-type alias Direction3d =
-  { x: Float
-  , y: Float
-  , z: Float
-  }
+type Direction3d
+  = Direction3d Vector3d
 
 
 -- Datums
 
 
-type alias Axis2d =
-  { originPoint: Point2d
-  , direction: Direction2d
-  }
+type Axis2d
+  = Axis2d Point2d Direction2d
 
 
-type alias Axis3d =
-  { originPoint: Point3d
-  , direction: Direction3d
-  }
+type Axis3d
+  = Axis3d Point3d Direction3d
 
 
-type alias Plane3d =
-  { originPoint: Point3d
-  , xDirection: Direction3d
-  , yDirection: Direction3d
-  , normalDirection: Direction3d
-  }
+type Plane3d
+  = Plane3d Point3d Direction3d Direction3d Direction3d
 
 
-type alias Frame2d =
-  { originPoint: Point2d
-  , xDirection: Direction2d
-  , yDirection: Direction2d
-  }
+type Frame2d
+  = Frame2d Point2d Direction2d Direction2d
 
 
-type alias Frame3d =
-  { originPoint: Point3d
-  , xDirection: Direction3d
-  , yDirection: Direction3d
-  , zDirection: Direction3d
-  }
+type Frame3d
+  = Frame3d Point3d Direction3d Direction3d Direction3d
 
 
 -- Transformations
 
 
-type alias Transformation2d =
-  ( Vector2d -> Vector2d
-  , Point2d -> Point2d
-  )
+type Transformation2d
+  = Transformation2d (Vector2d -> Vector2d) (Point2d -> Point2d)
 
 
-type alias Transformation3d =
-  ( Vector3d -> Vector3d
-  , Point3d -> Point3d
-  )
+type Transformation3d
+  = Transformation3d (Vector3d -> Vector3d) (Point3d -> Point3d)
 
 
 -- Simplices
 
 
-type alias LineSegment2d =
-  { firstEndpoint: Point2d
-  , secondEndpoint: Point2d
-  }
+type LineSegment2d
+  = LineSegment2d Point2d Point2d
 
 
-type alias LineSegment3d =
-  { firstEndpoint: Point3d
-  , secondEndpoint: Point3d
-  }
+type LineSegment3d
+  = LineSegment3d Point3d Point3d
 
 
-type alias Triangle2d =
-  { firstVertex: Point2d
-  , secondVertex: Point2d
-  , thirdVertex: Point2d
-  }
+type Triangle2d
+  = Triangle2d Point2d Point2d Point2d
 
 
-type alias Triangle3d =
-  { firstVertex: Point3d
-  , secondVertex: Point3d
-  , thirdVertex: Point3d
-  }
+type Triangle3d
+  = Triangle3d Point3d Point3d Point3d
 
 
 -- Bounds types
 
 
-type alias Interval =
-  { lowerBound: Float
-  , upperBound: Float
-  }
+type Interval
+  = Interval Float Float
 
 
-type alias Box2d =
-  { x: Interval
-  , y: Interval
-  }
+type Box2d
+  = Box2d Interval Interval
 
 
-type alias Box3d =
-  { x: Interval
-  , y: Interval
-  , z: Interval
-  }
+type Box3d
+  = Box3d Interval Interval Interval
