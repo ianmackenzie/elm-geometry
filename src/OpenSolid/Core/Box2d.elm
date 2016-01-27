dade6ea559e5b2b9eaa8fcd5b8da26a2e15e5e18
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2016-01-27 16:19:36 +1100

    Update Box*d modules to use union types

--- src/OpenSolid/Core/Box2d.elm
+++ src/OpenSolid/Core/Box2d.elm
@@ -5,20 +5,20 @@ module OpenSolid.Core.Box2d
   ) where
 
 
-import OpenSolid.Core exposing (Interval, Point2d, Box2d)
+import OpenSolid.Core exposing (..)
 import OpenSolid.Core.Interval as Interval
 
 
 components: Box2d -> (Interval, Interval)
-components box =
-  (box.x, box.y)
+components (Box2d x y) =
+  (x, y)
 
 
 contains: Point2d -> Box2d -> Bool
-contains point box =
-  Interval.contains point.x box.x && Interval.contains point.y box.y
+contains (Point2d pointX pointY) (Box2d x y) =
+  Interval.contains pointX x && Interval.contains pointY y
 
 
 overlaps: Box2d -> Box2d -> Bool
-overlaps other box =
-  Interval.overlaps other.x box.x && Interval.overlaps other.y box.y
+overlaps (Box2d otherX otherY) (Box2d x y) =
+  Interval.overlaps otherX x && Interval.overlaps otherY y
--- src/OpenSolid/Core/Box3d.elm
+++ src/OpenSolid/Core/Box3d.elm
@@ -5,24 +5,20 @@ module OpenSolid.Core.Box3d
   ) where
 
 
-import OpenSolid.Core exposing (Interval, Point3d, Box3d)
+import OpenSolid.Core exposing (..)
 import OpenSolid.Core.Interval as Interval
 
 
 components: Box3d -> (Interval, Interval, Interval)
-components box =
-  (box.x, box.y, box.z)
+components (Box3d x y z) =
+  (x, y, z)
 
 
 contains: Point3d -> Box3d -> Bool
-contains point box =
-  Interval.contains point.x box.x &&
-  Interval.contains point.y box.y &&
-  Interval.contains point.z box.z
+contains (Point3d pointX pointY pointZ) (Box3d x y z) =
+  Interval.contains pointX x && Interval.contains pointY y && Interval.contains pointZ z
 
 
 overlaps: Box3d -> Box3d -> Bool
-overlaps other box =
-  Interval.overlaps other.x box.x &&
-  Interval.overlaps other.y box.y &&
-  Interval.overlaps other.z box.z
+overlaps (Box3d otherX otherY otherZ) (Box3d x y z) =
+  Interval.overlaps otherX x && Interval.overlaps otherY y && Interval.overlaps otherZ z
