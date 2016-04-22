7126aadbec09265d6275d62661dddb8ca184be4c
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2016-04-22 22:51:13 +1000

    Refactor Scalar module

--- src/OpenSolid/Core/Scalar.elm
+++ src/OpenSolid/Core/Scalar.elm
@@ -1,8 +1,50 @@
 module OpenSolid.Core.Scalar
-  ( isZeroWithinTolerance
+  ( nan
+  , positiveInfinity
+  , negativeInfinity
+  , hullOf
+  , isZeroWithin
+  , hull
   ) where
 
 
-isZeroWithinTolerance: Float -> Float -> Bool
-isZeroWithinTolerance tolerance value =
+import List
+import Maybe
+import OpenSolid.Core exposing (..)
+
+
+nan: Float
+nan =
+  0 / 0
+
+
+positiveInfinity: Float
+positiveInfinity =
+  1 / 0
+
+
+negativeInfinity: Float
+negativeInfinity =
+  -1 / 0
+
+
+hullOf: List Float -> Interval
+hullOf values =
+  let
+    minValue = Maybe.withDefault nan (List.minimum values)
+    maxValue = Maybe.withDefault nan (List.maximum values)
+  in
+    Interval minValue maxValue
+
+
+isZeroWithin: Float -> Float -> Bool
+isZeroWithin tolerance value =
   -tolerance <= value && value <= tolerance
+
+
+hull: Float -> Float -> Interval
+hull firstValue secondValue =
+  if firstValue <= secondValue then
+    Interval firstValue secondValue
+  else
+    Interval secondValue firstValue
