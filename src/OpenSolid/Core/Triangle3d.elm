4686d219214e783ab42a5c7f38d1670c70485c88
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2016-05-05 14:13:19 +1000

    Update Triangle3d to use map/mapTo

--- src/OpenSolid/Core/Triangle3d.elm
+++ src/OpenSolid/Core/Triangle3d.elm
@@ -2,7 +2,8 @@ module OpenSolid.Core.Triangle3d
   ( fromVertices
   , vertices
   , edges
-  , mapReduce
+  , map
+  , mapTo
   , scaledAbout
   , projectedOnto
   , projectedInto
@@ -31,24 +32,38 @@ edges (Triangle3d p1 p2 p3) =
   (LineSegment3d p3 p2, LineSegment3d p1 p3, LineSegment3d p2 p1)
 
 
-mapReduce: (Point3d -> a) -> (a -> a -> a -> b) -> Triangle3d -> b
-mapReduce map reduce (Triangle3d p1 p2 p3) =
-  reduce (map p1) (map p2) (map p3)
+map: (Point3d -> Point3d) -> Triangle3d -> Triangle3d
+map =
+  mapTo Triangle3d
+
+
+mapTo: (a -> a -> a -> b) -> (Point3d -> a) -> Triangle3d -> b
+mapTo constructor map (Triangle3d p1 p2 p3) =
+  constructor (map p1) (map p2) (map p3)
 
 
 scaledAbout: Point3d -> Float -> Triangle3d -> Triangle3d
-scaledAbout point scale =
-  mapReduce (Point3d.scaledAbout point scale) Triangle3d
+scaledAbout centerPoint scale =
+  let
+    scalePoint = Point3d.scaledAbout centerPoint scale
+  in
+    map scalePoint
 
 
 projectedOnto: Plane3d -> Triangle3d -> Triangle3d
 projectedOnto plane =
-  mapReduce (Point3d.projectedOnto plane) Triangle3d
+  let
+    projectPoint = Point3d.projectedOnto plane
+  in
+    map projectPoint
 
 
 projectedInto: Plane3d -> Triangle3d -> Triangle2d
 projectedInto plane =
-  mapReduce (Point3d.projectedInto plane) Triangle2d
+  let
+    projectPoint = Point3d.projectedInto plane
+  in
+    mapTo Triangle2d projectPoint
 
 
 area: Triangle3d -> Float
