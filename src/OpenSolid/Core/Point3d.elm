87ad8d076ddc48e66318cc877ad9147d40d35de1
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2016-07-29 15:33:19 +1000

    Refactor Point3d documentation for readability

--- src/OpenSolid/Core/Point3d.elm
+++ src/OpenSolid/Core/Point3d.elm
@@ -10,9 +10,9 @@
 module OpenSolid.Core.Point3d
     exposing
         ( origin
-        , along
         , midpoint
         , interpolate
+        , along
         , coordinates
         , xCoordinate
         , yCoordinate
@@ -57,10 +57,10 @@ to numerical roundoff) they might not be exactly equal.
 # Constructors
 
 Since `Point3d` is not an opaque type, the simplest way to construct one is
-directly from its X, Y and Z coordinates, for example `Point2d ( 2, 1, 3 )`. But
-that is not the only way!
+directly from a tuple of its X, Y and Z coordinates, for example
+`Point2d ( 2, 1, 3 )`. But that is not the only way!
 
-@docs along, midpoint, interpolate
+@docs midpoint, interpolate, along
 
 # Coordinates
 
@@ -130,25 +130,15 @@ origin =
     Point3d ( 0, 0, 0 )
 
 
-{-| Construct a point along an axis at a particular distance from the axis'
-origin point. Positive and negative distances will be interpreted relative to
-the direction of the axis.
-
-    horizontalAxis =
-        Axis3d { originPoint = Point2d ( 1, 1, 1 ), direction = Direction3d.x }
-
-    Point3d.along horizontalAxis 3 == Point3d ( 4, 1, 1 )
-    Point3d.along horizontalAxis -3 == Point3d ( -2, 1, 1 )
-    Point3d.along (Axis3d.flip horizontalAxis) 3 == Point3d ( -2, 1, 1 )
--}
-along : Axis3d -> Float -> Point3d
-along (Axis3d { originPoint, direction }) distance =
-    translateBy (Direction3d.times distance direction) originPoint
-
-
 {-| Construct a point halfway between two other points.
 
-    Point3d.midpoint (Point3d ( 1, 1, 1 )) (Point3d ( 3, 7, 9 )) ==
+    p1 =
+        Point3d ( 1, 1, 1 )
+
+    p2 =
+        Point3d ( 3, 7, 9 )
+
+    Point3d.midpoint p1 p2 ==
         Point3d ( 2, 4, 5 )
 -}
 midpoint : Point3d -> Point3d -> Point3d
@@ -159,18 +149,34 @@ midpoint firstPoint secondPoint =
 {-| Construct a point by interpolating between two other points based on a
 parameter that ranges from zero to one.
 
+    startPoint =
+        Point3d ( 1, 1, 0 )
+
+    endPoint =
+        Point3d ( 1, 1, 8 )
+
+    Point3d.interpolate startPoint endPoint 0.25 ==
+        Point3d ( 1, 1, 2 )
+
+Partial application may be useful:
+
     interpolatedPoint : Float -> Point3d
     interpolatedPoint =
-        Point3d.interpolate Point3d.origin (Point3d ( 4, 8, 12 ))
+        Point3d.interpolate startPoint endPoint
 
-    interpolatedPoint 0 == Point3d ( 0, 0, 0 )
-    interpolatedPoint 0.25 == Point3d ( 1, 2, 3 )
-    interpolatedPoint 0.75 == Point3d ( 3, 6, 9 )
-    interpolatedPoint 1 == Point3d ( 4, 8, 12 )
+    List.map interpolatedPoint [ 0, 0.5, 1 ] ==
+        [ Point3d ( 1, 1, 0 )
+        , Point3d ( 1, 1, 4 )
+        , Point3d ( 1, 1, 8 )
+        ]
 
-You can actually pass values less than zero or greater than one to extrapolate:
+You can pass values less than zero or greater than one to extrapolate:
 
-    interpolatedPoint 1.25 == Point3d ( 5, 10, 15 )
+    interpolatedPoint -0.5 ==
+        Point3d ( 1, 1, -4 )
+
+    interpolatedPoint 1.25 ==
+        Point3d ( 1, 1, 10 )
 -}
 interpolate : Point3d -> Point3d -> Float -> Point3d
 interpolate startPoint endPoint =
@@ -181,9 +187,36 @@ interpolate startPoint endPoint =
         \t -> translateBy (Vector3d.times t displacement) startPoint
 
 
-{-| Get the (x, y, z) coordinates of a point as a tuple.
+{-| Construct a point along an axis at a particular distance from the axis'
+origin point.
 
-    ( x, y, z ) = Point3d.coordinates point
+    Point3d.along Axis3d.z 2 ==
+        Point3d ( 0, 0, 2 )
+
+Positive and negative distances are interpreted relative to the direction of the
+axis:
+
+    horizontalAxis =
+        Axis3d
+            { originPoint = Point2d ( 1, 1, 1 )
+            , direction = Direction3d.negate Direction3d.x
+            }
+
+    Point3d.along horizontalAxis 3 ==
+        Point3d ( -2, 1, 1 )
+
+    Point3d.along horizontalAxis -3 ==
+        Point3d ( 4, 1, 1 )
+-}
+along : Axis3d -> Float -> Point3d
+along (Axis3d { originPoint, direction }) distance =
+    translateBy (Direction3d.times distance direction) originPoint
+
+
+{-| Get the coordinates of a point as a tuple.
+
+    ( x, y, z ) =
+        Point3d.coordinates point
 -}
 coordinates : Point3d -> ( Float, Float, Float )
 coordinates (Point3d coordinates') =
@@ -219,7 +252,13 @@ zCoordinate (Point3d ( _, _, z )) =
 
 {-| Find the vector from one point to another.
 
-    Point3d.vectorFrom (Point3d ( 1, 1, 1 )) (Point3d ( 4, 5, 6 )) ==
+    startPoint =
+        Point3d ( 1, 1, 1 )
+
+    endPoint =
+        Point3d ( 4, 5, 6 )
+
+    Point3d.vectorFrom startPoint endPoint ==
         Vector3d ( 3, 4, 5 )
 -}
 vectorFrom : Point3d -> Point3d -> Vector3d
@@ -236,8 +275,11 @@ vectorFrom other point =
 
 {-| Flipped version of `vectorFrom`, where the end point is given first.
 
-    Point2d.vectorTo (Point2d ( 1, 1, 1 )) (Point2d ( 4, 5, 6 )) ==
-        Vector2d ( -3, -4, -5 )
+    startPoint =
+        Point3d ( 2, 1, 3 )
+
+    Point2d.vectorTo Point3d.origin startPoint ==
+        Vector2d ( -2, -1, -3 )
 -}
 vectorTo : Point3d -> Point3d -> Vector3d
 vectorTo =
@@ -246,15 +288,31 @@ vectorTo =
 
 {-| Find the distance between two points.
 
-    Point3d.distanceFrom (Point3d ( 1, 1, 1 )) (Point3d ( 2, 2, 2 )) == sqrt 3
+    p1 =
+        Point3d ( 1, 1, 1 )
+
+    p2 =
+        Point3d ( 2, 2, 2 )
+
+    Point3d.distanceFrom p1 p2 == sqrt 3
 
 Partial application can be useful:
 
     points =
-        [ Point3d ( 3, 4, 5 ), Point3d ( 10, 10, 10 ), Point3d ( -1, 2, -3 ) ]
+        [ Point3d ( 3, 4, 5 )
+        , Point3d ( 10, 10, 10 )
+        , Point3d ( -1, 2, -3 )
+        ]
 
-    List.sortBy (Point3d.distanceFrom Point3d.origin) points ==
-        [ Point3d ( -1, 2, -3 ), Point3d ( 3, 4, 5 ), Point3d ( 10, 10, 10 ) ]
+    distanceFromOrigin : Point3d -> Float
+    distanceFromOrigin =
+        Point3d.distanceFrom Point3d.origin
+
+    List.sortBy distanceFromOrigin points ==
+        [ Point3d ( -1, 2, -3 )
+        , Point3d ( 3, 4, 5 )
+        , Point3d ( 10, 10, 10 )
+        ]
 -}
 distanceFrom : Point3d -> Point3d -> Float
 distanceFrom other =
@@ -264,11 +322,11 @@ distanceFrom other =
 {-| Find the square of the distance from one point to another.
 `squaredDistanceFrom` is slightly faster than `distanceFrom`, so for example
 
-    Point3d.squaredDistanceFrom firstPoint secondPoint > tolerance * tolerance
+    Point3d.squaredDistanceFrom p1 p2 > tolerance * tolerance
 
 is equivalent to but slightly more efficient than
 
-    Point3d.distanceFrom firstPoint secondPoint > tolerance
+    Point3d.distanceFrom p1 p2 > tolerance
 
 since the latter requires a square root under the hood. In many cases, however,
 the speed difference will be negligible and using `distanceFrom` is much more
@@ -281,9 +339,12 @@ squaredDistanceFrom other =
 
 {-| Find the perpendicular (nearest) distance of a point from an axis.
 
-    Point3d.distanceFromAxis Axis3d.x (Point3d ( -1, 2, 0 )) == 2
-    Point3d.distanceFromAxis Axis3d.y (Point3d ( -1, 2, 0 )) == 1
-    Point3d.distanceFromAxis Axis3d.z (Point3d ( -1, 2, 0 )) == sqrt 5
+    point =
+        Point3d ( -1, 2, 0 )
+
+    Point3d.distanceFromAxis Axis3d.x point == 2
+    Point3d.distanceFromAxis Axis3d.y point == 1
+    Point3d.distanceFromAxis Axis3d.z point == sqrt 5
 
 Note that unlike in 2D, the result is always positive (unsigned) since there is
 no such thing as the left or right side of an axis in 3D.
@@ -318,9 +379,15 @@ positive if the projected point is ahead the axis' origin point and negative if
 it is behind, with 'ahead' and 'behind' defined by the direction of the axis.
 
     axis =
-        Axis3d { originPoint = Point3d ( 1, 0, 0 ), direction = Direction3d.x }
+        Axis3d
+            { originPoint = Point3d ( 1, 0, 0 )
+            , direction = Direction3d.x
+            }
 
-    Point3d.signedDistanceAlong axis (Point3d ( 3, 3, 3 )) == 2
+    point =
+        Point3d ( 3, 3, 3 )
+
+    Point3d.signedDistanceAlong axis point == 2
     Point3d.signedDistanceAlong axis Point3d.origin == -1
 -}
 signedDistanceAlong : Axis3d -> Point3d -> Float
@@ -334,9 +401,7 @@ signedDistanceAlong axis =
 
 {-| Find the perpendicular distance of a point from a plane. The result will be
 positive if the point is 'above' the plane and negative if it is 'below', with
-'up' defined by the normal direction of the plane. This means that flipping a
-plane (reversing its normal direction) will also flip the sign of the result of
-this function.
+'up' defined by the normal direction of the plane.
 
     plane =
         Plane3d
@@ -344,11 +409,19 @@ this function.
             , normalDirection = Direction2d.y
             }
 
-    Point3d.signedDistanceFrom plane (Point3d ( 3, 3, 3 )) == 1
+    point =
+        Point3d ( 3, 3, 3 )
+
+    Point3d.signedDistanceFrom plane point == 1
     Point3d.signedDistanceFrom plane Point3d.origin == -2
 
-    Point3d.signedDistanceFrom (Plane3d.flip plane) (Point3d ( 3, 3, 3 )) == -1
-    Point3d.signedDistanceFrom (Plane3d.flip plane) Point3d.origin == 2
+This means that flipping a plane (reversing its normal direction) will also flip
+the sign of the result of this function:
+
+    flippedPlane =
+        Plane3d.flip plane
+
+    Point3d.signedDistanceFrom flippedPlane point == -1
 -}
 signedDistanceFrom : Plane3d -> Point3d -> Float
 signedDistanceFrom plane =
@@ -364,17 +437,23 @@ given first and the point to transform is given last. Points will contract or
 expand about the center point by the given scale. Scaling by a factor of 1 is a
 no-op, and scaling by a factor of 0 collapses all points to the center point.
 
-    Point3d.scaleAbout Point3d.origin 3 (Point3d ( 1, 2, 3 )) ==
+    centerPoint =
+        Point3d ( 1, 1, 1 )
+
+    point =
+        Point3d ( 1, 2, 3 )
+
+    Point3d.scaleAbout Point3d.origin 3 point ==
         Point3d ( 3, 6, 9 )
 
-    Point3d.scaleAbout (Point3d ( 1, 1, 1 )) 2 (Point3d ( 1, 2, 3 )) ==
-        Point3d ( 1, 3, 5 )
+    Point3d.scaleAbout centerPoint 3 point ==
+        Point3d ( 1, 4, 7 )
 
-    Point3d.scaleAbout (Point3d ( 1, 1, 1 )) 0.5 (Point3d ( 1, 2, 3 )) ==
+    Point3d.scaleAbout centerPoint 0.5 point ==
         Point3d ( 1, 1.5, 2 )
 
-    Point3d.scaleAbout (Point3d ( 1, 1, 1 )) 10 (Point3d ( 1, 1, 1 )) ==
-        Point3d ( 1, 1, 1 )
+    Point3d.scaleAbout centerPoint 10 centerPoint ==
+        centerPoint
 
 Do not scale by a negative scaling factor - while this may sometimes do what you
 want it is confusing and error prone. Try a combination of mirror and/or
@@ -387,11 +466,17 @@ scaleAbout centerPoint scale =
 
 {-| Rotate a point around an axis by a given angle (in radians).
 
-    Point3d.rotateAround Axis3d.x (degrees 90) (Point3d ( 3, 1, 0 )) ==
-        Point3d ( 3, 0, 1 )
+    axis =
+        Axis3d.x
 
-    Point3d.rotateAround Axis3d.z (degrees 45) (Point3d ( 1, 0, 0 )) ==
-        Point3d ( 0.7071, 0.7071, 0 )
+    angle =
+        degrees 45
+
+    point =
+        Point3d ( 3, 1, 0 )
+
+    Point3d.rotateAround axis angle point ==
+        Point3d ( 3, 0.7071, 0.7071 )
 
 Rotation direction is given by the right-hand rule, counterclockwise about the
 direction of the axis.
@@ -409,7 +494,13 @@ rotateAround axis angle =
 
 {-| Translate a point by a given displacement. You can think of this as 'plus'.
 
-    Point3d.translateBy (Vector3d ( 1, 2, 3 )) (Point3d ( 3, 4, 5 )) ==
+    point =
+        Point3d ( 3, 4, 5 )
+
+    displacement =
+        Vector3d ( 1, 2, 3 )
+
+    Point3d.translateBy displacement point ==
         Point3d ( 4, 6, 8 )
 -}
 translateBy : Vector3d -> Point3d -> Point3d
@@ -427,12 +518,18 @@ translateBy vector point =
 {-| Mirror a point across a plane. The result will be the same distance from the
 plane but on the opposite side.
 
-    Point3d.mirrorAcross Plane3d.xy (Point3d ( 1, 2, 3 )) ==
+    point =
+        Point3d ( 1, 2, 3 )
+
+    -- Plane3d.xy is the plane Z=0
+    Point3d.mirrorAcross Plane3d.xy point ==
         Point3d ( 1, 2, -3 )
 
-    Point3d.mirrorAcross Plane3d.yz (Point3d ( 1, 2, 3 )) ==
+    -- Plane3d.yz is the plane X=0
+    Point3d.mirrorAcross Plane3d.yz point ==
         Point3d ( -1, 2, 3 )
 
+    -- offsetPlane is the plane Z=1
     offsetPlane =
         Plane3d.offsetBy 1 Plane3d.xy
 
@@ -454,16 +551,19 @@ mirrorAcross plane =
 
 {-| Project a point perpendicularly onto a plane.
 
-    Point3d.projectOnto Plane3d.xy (Point3d ( 1, 2, 3 )) ==
+    point =
+        Point3d ( 1, 2, 3 )
+
+    Point3d.projectOnto Plane3d.xy point ==
         Point3d ( 1, 2, 0 )
 
-    Point3d.projectOnto Plane3d.yz (Point3d ( 1, 2, 3 )) ==
+    Point3d.projectOnto Plane3d.yz point ==
         Point3d ( 0, 2, 3 )
 
     offsetPlane =
         Plane3d.offsetBy 1 Plane3d.xy
 
-    Point3d.projectOnto offsetPlane (Point3d ( 1, 2, 3 )) ==
+    Point3d.projectOnto offsetPlane point ==
         Point3d ( 1, 2, 1 )
 -}
 projectOnto : Plane3d -> Point3d -> Point3d
@@ -483,16 +583,27 @@ projectOnto plane point =
 
 {-| Project a point perpendicularly onto an axis.
 
-    Point3d.projectOntoAxis Axis2d.x (Point3d ( 1, 2, 3 )) ==
+    point =
+        Point3d ( 1, 2, 3 )
+
+    Point3d.projectOntoAxis Axis2d.x point ==
         Point2d ( 1, 0, 0 )
 
-    Point3d.projectOntoAxis Axis2d.y (Point3d ( 1, 2, 3 )) ==
+    Point3d.projectOntoAxis Axis2d.y point ==
         Point2d ( 0, 2, 0 )
 
-    verticalAxis =
-        Axis3d { originPoint = Point3d ( 1, 1, 1 ), direction = Direction3d.z }
+The axis does not have to pass through the origin:
 
-    Point3d.projectOntoAxis verticalAxis (Point3d ( 10, 10, 10 )) ==
+    point =
+        Point3d ( 10, 10, 10 )
+
+    offsetVerticalAxis =
+        Axis3d
+            { originPoint = Point3d ( 1, 1, 1 )
+            , direction = Direction3d.z
+            }
+
+    Point3d.projectOntoAxis offsetVerticalAxis point ==
         Point3d ( 1, 1, 10 )
 -}
 projectOntoAxis : Axis3d -> Point3d -> Point3d
@@ -508,16 +619,19 @@ projectOntoAxis axis =
 
 {-| Project a point into a given planar frame. Conceptually, this projects the
 point onto the plane of the given frame and then expresses the projected point
-in terms of 2D components within the plane (relative to the given frame's X and
+in terms of 2D coordinates within the plane (relative to the given frame's X and
 Y axes).
 
-    Point3d.projectInto2d PlanarFrame3d.xy (Point3d ( 2, 1, 3 )) ==
+    point =
+        Point3d ( 2, 1, 3 )
+
+    Point3d.projectInto2d PlanarFrame3d.xy point ==
         Point2d ( 2, 1 )
 
-    Point3d.projectInto2d PlanarFrame3d.yz (Point3d ( 2, 1, 3 )) ==
+    Point3d.projectInto2d PlanarFrame3d.yz point ==
         Point2d ( 1, 3 )
 
-    Point3d.projectInto2d PlanarFrame3d.zx (Point3d ( 2, 1, 3 )) ==
+    Point3d.projectInto2d PlanarFrame3d.zx point ==
         Point2d ( 3, 2 )
 -}
 projectInto2d : PlanarFrame3d -> Point3d -> Point2d
@@ -535,11 +649,17 @@ projectInto2d planarFrame =
 frame. The result will be the given point expressed relative to the given
 frame.
 
-    localFrame =
-        Frame3d.translateTo (Point3d ( 1, 2, 3 )) Frame3d.xyz
+    localOrigin =
+        Point3d ( 1, 2, 3 )
 
-    Point3d.localizeTo localFrame (Point3d ( 4, 5, 6 )) == Point3d ( 3, 3, 3 )
-    Point3d.localizeTo localFrame (Point3d ( 1, 1, 1 )) == Point3d ( 0, -1, -2 )
+    localFrame =
+        Frame3d.moveTo localOrigin Frame3d.xyz
+
+    Point3d.localizeTo localFrame (Point3d ( 4, 5, 6 )) ==
+        Point3d ( 3, 3, 3 )
+
+    Point3d.localizeTo localFrame (Point3d ( 1, 1, 1 )) ==
+        Point3d ( 0, -1, -2 )
 -}
 localizeTo : Frame3d -> Point3d -> Point3d
 localizeTo frame =
@@ -555,11 +675,17 @@ localizeTo frame =
 {-| Convert a point from local coordinates within a given frame to global
 coordinates. Inverse of `localizeTo`.
 
-    localFrame =
-        Frame3d.translateTo (Point3d ( 1, 2, 3 )) Frame3d.xy
+    localOrigin =
+        Point3d ( 1, 2, 3 )
 
-    Point3d.placeIn localFrame (Point3d ( 3, 3, 3 )) == Point3d ( 4, 5, 6 )
-    Point3d.placeIn localFrame (Point3d ( 0, -1, -2 )) == Point3d ( 1, 1, 1 )
+    localFrame =
+        Frame3d.moveTo localOrigin Frame3d.xyz
+
+    Point3d.placeIn localFrame (Point3d ( 3, 3, 3 )) ==
+        Point3d ( 4, 5, 6 )
+
+    Point3d.placeIn localFrame (Point3d ( 0, -1, -2 )) ==
+        Point3d ( 1, 1, 1 )
 -}
 placeIn : Frame3d -> Point3d -> Point3d
 placeIn frame =
@@ -572,7 +698,8 @@ placeIn frame =
 
 {-| Convert a point to a record with `x`, `y` and `z` fields.
 
-    Point3d.toRecord (Point3d ( 2, 3, 1 )) == { x = 2, y = 3, z = 1 }
+    Point3d.toRecord (Point3d ( 2, 3, 1 )) ==
+        { x = 2, y = 3, z = 1 }
 -}
 toRecord : Point3d -> { x : Float, y : Float, z : Float }
 toRecord (Point3d ( x, y, z )) =
@@ -581,7 +708,8 @@ toRecord (Point3d ( x, y, z )) =
 
 {-| Construct a point from a record with `x`, `y` and `z` fields.
 
-    Point3d.fromRecord { x = 2, y = 3, z = 1 } == Point2d ( 2, 3, 1 )
+    Point3d.fromRecord { x = 2, y = 3, z = 1 } ==
+        Point2d ( 2, 3, 1 )
 -}
 fromRecord : { x : Float, y : Float, z : Float } -> Point3d
 fromRecord { x, y, z } =
