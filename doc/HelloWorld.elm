1795485eb127a0c8158b2ec72c65dfd6ae0ecf2d
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2016-08-17 14:29:51 +1000

    Update doc test image to use OpenSolid.Svg module

--- doc/HelloWorld.elm
+++ doc/HelloWorld.elm
@@ -1,9 +1,45 @@
 module HelloWorld exposing (..)
 
-import Html
+import Html exposing (Html)
+import Svg exposing (Svg)
 import OpenSolid.Core.Types exposing (..)
-import OpenSolid.Core.Vector2d as Vector2d
+import OpenSolid.Core.Point2d as Point2d
+import OpenSolid.BoundingBox.Types exposing (..)
+import OpenSolid.Svg as Svg
+import Svg.Attributes as Attributes
 
 
+angles =
+    [0..24] |> List.map (\index -> index * degrees 15)
+
+
+firstPoint =
+    Point2d ( 100, 0 )
+
+
+pointAt angle =
+    Point2d.rotateAround Point2d.origin angle firstPoint
+
+
+points =
+    List.map pointAt angles
+
+
+svgPoint : Point2d -> Svg Never
+svgPoint point =
+    Svg.point2d [ Attributes.r "2", Attributes.fill "blue" ] point
+
+
+pointsGroup : Svg Never
+pointsGroup =
+    Svg.g [] (List.map svgPoint points)
+
+
+boundingBox : BoundingBox2d
+boundingBox =
+    BoundingBox2d { minX = -120, minY = -120, maxX = 120, maxY = 120 }
+
+
+main : Html Never
 main =
-    Html.text (toString (Vector2d.length (Vector2d ( 2, 3 ))))
+    Svg.scene2d boundingBox [ pointsGroup ]
