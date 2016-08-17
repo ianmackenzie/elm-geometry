5f30dcd200ef18f6a9b854c0bdd8abf5a18e75ec
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2016-08-17 22:25:40 +1000

    Reorganize image script for readability

--- doc/build.js
+++ doc/build.js
@@ -32,20 +32,21 @@ if (!shell.test("-d", "output")) {
 names.forEach(function(name) {
     let elmFilename = name + ".elm";
     let htmlFilename = "output/" + name + ".html";
-    let elmMakeCommand = "elm make --yes --output " + htmlFilename + " " + elmFilename;
+    let pngFilename = "output/" + name + ".png";
+
     // Compile Elm file to HTML
-    if (shell.exec(elmMakeCommand).code == 0) {
-        // Render HTML to PNG
-        let nightmare = Nightmare({show: false}).viewport(1024, 768);
-        let pngFilename = "output/" + name + ".png";
-        let dimensions = {x: 0, y: 0, width: 240, height : 240};
-        let url = fileUrl(htmlFilename);
-        nightmare.goto(url).screenshot(pngFilename, dimensions).end().then(function (result) {
-            console.log("Rendered " + pngFilename);
-        }).catch(function (error) {
-            console.error("Render failed:", error);
-        });
-    } else {
+    let elmMakeCommand = "elm make --yes --output " + htmlFilename + " " + elmFilename;
+    if (shell.exec(elmMakeCommand).code != 0)  {
         shell.echo("Elm compilation failed");
+        process.exit(1);
     }
+
+    // Render HTML to PNG
+    let nightmare = Nightmare({show: false}).viewport(1024, 768);
+    let dimensions = {x: 0, y: 0, width: 240, height : 240};
+    nightmare.goto(fileUrl(htmlFilename)).screenshot(pngFilename, dimensions).end().then(function (result) {
+        console.log("Rendered " + pngFilename);
+    }).catch(function (error) {
+        console.error("Render failed:", error);
+    });
 })
