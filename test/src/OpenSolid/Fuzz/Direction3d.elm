7142711b9607a37f81408b4317b8b34b6ddc8874
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2016-09-04 01:06:22 +1000

    Simplify Direction#d fuzzers

--- test/src/OpenSolid/Fuzz/Direction3d.elm
+++ test/src/OpenSolid/Fuzz/Direction3d.elm
@@ -1,43 +1,26 @@
 module OpenSolid.Fuzz.Direction3d exposing (direction3d)
 
 import Fuzz exposing (Fuzzer)
-import Random.Pcg as Random
-import Shrink
 import OpenSolid.Types exposing (..)
-import OpenSolid.Vector3d as Vector3d
-import OpenSolid.Fuzz as Fuzz
 
 
 direction3d : Fuzzer Direction3d
 direction3d =
     let
-        componentGenerator =
-            Random.float -2 2
+        thetaFuzzer =
+            Fuzz.floatRange -pi pi
 
-        vectorGenerator =
-            Random.map3 (\x y z -> Vector3d ( x, y, z ))
-                componentGenerator
-                componentGenerator
-                componentGenerator
+        t =
+            Fuzz.floatRange -1 1
 
-        isValid vector =
-            let
-                length =
-                    Vector3d.length vector
-            in
-                length >= 0.5 && length <= 2
+        phiFuzzer =
+            Fuzz.map acos t
 
-        validVectorGenerator =
-            Random.filter isValid vectorGenerator
-
-        directionOf validVector =
-            let
-                normalizedVector =
-                    Vector3d.times (1 / Vector3d.length validVector) validVector
-            in
-                Direction3d (Vector3d.components normalizedVector)
-
-        directionGenerator =
-            Random.map directionOf validVectorGenerator
+        direction ( theta, phi ) =
+            Direction3d
+                ( sin phi * cos theta
+                , sin phi * sin theta
+                , cos phi
+                )
     in
-        Fuzz.custom directionGenerator Shrink.noShrink
+        Fuzz.map direction (Fuzz.tuple ( thetaFuzzer, phiFuzzer ))
