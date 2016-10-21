f860cacd972a6bcab101cf2a10c49a136ab60dba
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2016-10-21 18:39:58 +1100

    Tweak documentation wording

--- src/OpenSolid/Frame2d.elm
+++ src/OpenSolid/Frame2d.elm
@@ -360,7 +360,7 @@ translateAlongOwn localAxis distance frame =
             , yDirection = Direction2d.negate Direction2d.y
             }
 
-Note that this will change the
+Note that this will switch the
 [handedness](https://en.wikipedia.org/wiki/Cartesian_coordinate_system#Orientation_and_handedness)
 of the frame.
 -}
