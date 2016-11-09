16554f882b2c8739eb7e1a4698038feaf788b8f1
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2016-11-09 16:37:01 +1100

    Start documenting Triangle2d

--- src/OpenSolid/Triangle2d.elm
+++ src/OpenSolid/Triangle2d.elm
@@ -14,19 +14,80 @@ module OpenSolid.Triangle2d
     exposing
         ( vertices
         , edges
-        , map
+        , centroid
         , contains
+        , area
+        , counterclockwiseArea
+        , clockwiseArea
         , scaleAbout
         , rotateAround
         , translateBy
         , mirrorAcross
+        , map
+        , relativeTo
+        , placeIn
         , placeOnto
-        , area
-        , clockwiseArea
-        , counterclockwiseArea
-        , centroid
         , boundingBox
         )
+
+{-| Various functions for creating and working with `Triangle2d` values.
+Triangles are defined by their three vertices and can be constructed by passing
+a tuple of vertices to the `Triangle2d` constructor, for example
+
+    exampleTriangle =
+        Triangle2d
+            ( Point2d ( 1, 1 )
+            , Point2d ( 2, 1 )
+            , Point2d ( 1, 3 )
+            )
+
+## Reading this documentation
+
+For the examples below, assume that `exampleTriangle` has been defined as above,
+all OpenSolid core types have been imported using
+
+    import OpenSolid.Geometry.Types exposing (..)
+
+and all other necessary modules have been imported using the following pattern:
+
+    import OpenSolid.Triangle2d as Triangle2d
+
+Examples use `==` to indicate that two expressions are equivalent, even if (due
+to numerical roundoff) they might not be exactly equal.
+
+# Accessors
+
+@docs vertices, edges
+
+# Basics
+
+@docs centroid, contains
+
+# Area
+
+@docs area, counterclockwiseArea, clockwiseArea
+
+# Transformations
+
+Transforming a triangle is equivalent to transforming its vertices.
+
+@docs scaleAbout, rotateAround, translateBy, mirrorAcross, map
+
+# Coordinate frames
+
+Functions for transforming triangles between local and global coordinates in
+different coordinate frames.
+
+@docs relativeTo, placeIn
+
+# Sketch planes
+
+@docs placeOnto
+
+# Bounds
+
+@docs boundingBox
+-}
 
 import OpenSolid.Geometry.Types exposing (..)
 import OpenSolid.Vector2d as Vector2d
@@ -50,13 +111,22 @@ edges triangle =
         )
 
 
-map : (Point2d -> Point2d) -> Triangle2d -> Triangle2d
-map function triangle =
+centroid : Triangle2d -> Point2d
+centroid triangle =
     let
         ( p1, p2, p3 ) =
             vertices triangle
+
+        firstVector =
+            Point2d.vectorFrom p1 p2
+
+        secondVector =
+            Point2d.vectorFrom p1 p3
+
+        displacement =
+            Vector2d.times (1.0 / 3.0) (Vector2d.plus secondVector firstVector)
     in
-        Triangle2d ( function p1, function p2, function p3 )
+        Point2d.translateBy displacement p1
 
 
 contains : Point2d -> Triangle2d -> Bool
@@ -88,6 +158,31 @@ contains point triangle =
             || (firstProduct <= 0 && secondProduct <= 0 && thirdProduct <= 0)
 
 
+area : Triangle2d -> Float
+area =
+    counterclockwiseArea >> abs
+
+
+clockwiseArea : Triangle2d -> Float
+clockwiseArea triangle =
+    -(counterclockwiseArea triangle)
+
+
+counterclockwiseArea : Triangle2d -> Float
+counterclockwiseArea triangle =
+    let
+        ( p1, p2, p3 ) =
+            vertices triangle
+
+        firstVector =
+            Point2d.vectorFrom p1 p2
+
+        secondVector =
+            Point2d.vectorFrom p1 p3
+    in
+        0.5 * Vector2d.crossProduct firstVector secondVector
+
+
 scaleAbout : Point2d -> Float -> Triangle2d -> Triangle2d
 scaleAbout point scale =
     map (Point2d.scaleAbout point scale)
@@ -108,6 +203,25 @@ mirrorAcross axis =
     map (Point2d.mirrorAcross axis)
 
 
+map : (Point2d -> Point2d) -> Triangle2d -> Triangle2d
+map function triangle =
+    let
+        ( p1, p2, p3 ) =
+            vertices triangle
+    in
+        Triangle2d ( function p1, function p2, function p3 )
+
+
+relativeTo : Frame2d -> Triangle2d -> Triangle2d
+relativeTo frame =
+    map (Point2d.relativeTo frame)
+
+
+placeIn : Frame2d -> Triangle2d -> Triangle2d
+placeIn frame =
+    map (Point2d.placeIn frame)
+
+
 placeOnto : SketchPlane3d -> Triangle2d -> Triangle3d
 placeOnto sketchPlane =
     let
@@ -120,59 +234,6 @@ placeOnto sketchPlane =
                     vertices triangle
             in
                 Triangle3d ( place p1, place p2, place p3 )
-
-
-area : Triangle2d -> Float
-area =
-    clockwiseArea >> abs
-
-
-clockwiseArea : Triangle2d -> Float
-clockwiseArea triangle =
-    let
-        ( p1, p2, p3 ) =
-            vertices triangle
-
-        firstVector =
-            Point2d.vectorFrom p1 p2
-
-        secondVector =
-            Point2d.vectorFrom p1 p3
-    in
-        0.5 * Vector2d.crossProduct secondVector firstVector
-
-
-counterclockwiseArea : Triangle2d -> Float
-counterclockwiseArea triangle =
-    let
-        ( p1, p2, p3 ) =
-            vertices triangle
-
-        firstVector =
-            Point2d.vectorFrom p1 p2
-
-        secondVector =
-            Point2d.vectorFrom p1 p3
-    in
-        0.5 * Vector2d.crossProduct firstVector secondVector
-
-
-centroid : Triangle2d -> Point2d
-centroid triangle =
-    let
-        ( p1, p2, p3 ) =
-            vertices triangle
-
-        firstVector =
-            Point2d.vectorFrom p1 p2
-
-        secondVector =
-            Point2d.vectorFrom p1 p3
-
-        displacement =
-            Vector2d.times (1.0 / 3.0) (Vector2d.plus secondVector firstVector)
-    in
-        Point2d.translateBy displacement p1
 
 
 boundingBox : Triangle2d -> BoundingBox2d
