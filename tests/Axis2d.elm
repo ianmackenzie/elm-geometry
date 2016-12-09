8025fe78ba15395cd07a0a410c0240c8e2e11282
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2016-12-09 17:20:37 -0500

    Finish testing Axis2d documentation examples

--- tests/Axis2d.elm
+++ tests/Axis2d.elm
@@ -18,6 +18,8 @@ import OpenSolid.Geometry.Types exposing (..)
 import OpenSolid.Axis2d as Axis2d
 import OpenSolid.Point2d as Point2d
 import OpenSolid.Direction2d as Direction2d
+import OpenSolid.Frame2d as Frame2d
+import OpenSolid.SketchPlane3d as SketchPlane3d
 import OpenSolid.Geometry.Encode as Encode
 import OpenSolid.Geometry.Decode as Decode
 import OpenSolid.Geometry.Fuzz as Fuzz
@@ -119,6 +121,116 @@ moveToExample =
                         )
 
 
+rotateAroundExample : Test
+rotateAroundExample =
+    Test.test "Axis2d.rotateAround example" <|
+        \() ->
+            Axis2d.rotateAround Point2d.origin (degrees 90) Axis2d.x
+                |> Expect.axis2d Axis2d.y
+
+
+translateByExample : Test
+translateByExample =
+    Test.test "Axis2d.translateBy example" <|
+        \() ->
+            let
+                displacement =
+                    Vector2d ( 2, 3 )
+            in
+                Axis2d.translateBy displacement Axis2d.y
+                    |> Expect.axis2d
+                        (Axis2d
+                            { originPoint = Point2d ( 2, 3 )
+                            , direction = Direction2d.y
+                            }
+                        )
+
+
+mirrorAcrossExample : Test
+mirrorAcrossExample =
+    Test.test "Axis2d.mirrorAcross example" <|
+        \() ->
+            let
+                axis =
+                    Axis2d
+                        { originPoint = Point2d ( 1, 2 )
+                        , direction = Direction2d.fromAngle (degrees 30)
+                        }
+            in
+                Axis2d.mirrorAcross Axis2d.x axis
+                    |> Expect.axis2d
+                        (Axis2d
+                            { originPoint = Point2d ( 1, -2 )
+                            , direction = Direction2d.fromAngle (degrees -30)
+                            }
+                        )
+
+
+relativeToExample : Test
+relativeToExample =
+    Test.test "Axis2d.relativeTo example" <|
+        \() ->
+            let
+                originPoint =
+                    Point2d ( 2, 3 )
+            in
+                Axis2d.relativeTo (Frame2d.at originPoint) Axis2d.x
+                    |> Expect.axis2d
+                        (Axis2d
+                            { originPoint = Point2d ( -2, -3 )
+                            , direction = Direction2d.x
+                            }
+                        )
+
+
+placeInExample : Test
+placeInExample =
+    Test.test "Axis2d.placeIn example" <|
+        \() ->
+            let
+                originPoint =
+                    Point2d ( 2, 3 )
+            in
+                Axis2d.placeIn (Frame2d.at originPoint) Axis2d.x
+                    |> Expect.axis2d
+                        (Axis2d
+                            { originPoint = Point2d ( 2, 3 )
+                            , direction = Direction2d.x
+                            }
+                        )
+
+
+placeOntoExamples : Test
+placeOntoExamples =
+    let
+        axis =
+            Axis2d
+                { originPoint = Point2d ( 2, 3 )
+                , direction = Direction2d ( 0.6, 0.8 )
+                }
+    in
+        Test.describe "Axis2d.placeOnto examples"
+            [ Test.test "First example" <|
+                \() ->
+                    Axis2d.placeOnto SketchPlane3d.xy axis
+                        |> Expect.axis3d
+                            (Axis3d
+                                { originPoint = Point3d ( 2, 3, 0 )
+                                , direction = Direction3d ( 0.6, 0.8, 0 )
+                                }
+                            )
+            , Test.test "Second example" <|
+                \() ->
+                    Axis2d.placeOnto SketchPlane3d.zx axis
+                        |> Expect.axis3d
+                            (Axis3d
+                                { originPoint = Point3d ( 3, 0, 2 )
+                                , direction = Direction3d ( 0.8, 0, 0.6 )
+                                }
+                            )
+            ]
+
+
 documentationExamples : Test
 documentationExamples =
     Test.describe "Documentation examples are correct"
@@ -129,6 +241,12 @@ documentationExamples =
         , directionExample
         , flipExample
         , moveToExample
+        , rotateAroundExample
+        , translateByExample
+        , mirrorAcrossExample
+        , relativeToExample
+        , placeInExample
+        , placeOntoExamples
         ]
 
 
