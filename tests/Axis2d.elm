be3dbb11f58d2d79fe702aa40bc033163f25537c
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2016-12-09 15:19:19 -0500

    Start adding documentation example tests for Axis2d
    Simple tests to verify that examples in documentation are actually
    correct, and to sanity-check basic functionality

--- tests/Axis2d.elm
+++ tests/Axis2d.elm
@@ -14,7 +14,10 @@ module Axis2d exposing (suite)
 
 import Test exposing (Test)
 import Test.Runner.Html as HtmlRunner
+import OpenSolid.Geometry.Types exposing (..)
 import OpenSolid.Axis2d as Axis2d
+import OpenSolid.Point2d as Point2d
+import OpenSolid.Direction2d as Direction2d
 import OpenSolid.Geometry.Encode as Encode
 import OpenSolid.Geometry.Decode as Decode
 import OpenSolid.Geometry.Fuzz as Fuzz
@@ -27,10 +30,113 @@ jsonRoundTrips =
     Generic.jsonRoundTrips Fuzz.axis2d Encode.axis2d Decode.axis2d
 
 
+xExample : Test
+xExample =
+    Test.test "Axis2d.x example" <|
+        \() ->
+            Axis2d.x
+                |> Expect.axis2d
+                    (Axis2d
+                        { originPoint = Point2d.origin
+                        , direction = Direction2d.x
+                        }
+                    )
+
+
+yExample : Test
+yExample =
+    Test.test "Axis2d.y example" <|
+        \() ->
+            Axis2d.y
+                |> Expect.axis2d
+                    (Axis2d
+                        { originPoint = Point2d.origin
+                        , direction = Direction2d.y
+                        }
+                    )
+
+
+perpendicularToExamples : Test
+perpendicularToExamples =
+    Test.describe "Axis2d.perpendicularTo examples"
+        [ Test.test "First example" <|
+            \() ->
+                Axis2d.perpendicularTo Axis2d.x |> Expect.axis2d Axis2d.y
+        , Test.test "Second example" <|
+            \() ->
+                Axis2d.perpendicularTo Axis2d.y
+                    |> Expect.axis2d (Axis2d.flip Axis2d.x)
+        ]
+
+
+originPointExample : Test
+originPointExample =
+    Test.test "Axis2d.originPoint example" <|
+        \() ->
+            Axis2d.originPoint Axis2d.x |> Expect.point2d Point2d.origin
+
+
+directionExample : Test
+directionExample =
+    Test.test "Axis2d.direction example" <|
+        \() ->
+            Axis2d.direction Axis2d.y |> Expect.direction2d Direction2d.y
+
+
+flipExample : Test
+flipExample =
+    Test.test "Axis2d.flip example" <|
+        \() ->
+            Axis2d.flip Axis2d.x
+                |> Expect.axis2d
+                    (Axis2d
+                        { originPoint = Point2d.origin
+                        , direction = Direction2d.negate Direction2d.x
+                        }
+                    )
+
+
+moveToExample : Test
+moveToExample =
+    Test.test "Axis2d.moveTo example" <|
+        \() ->
+            let
+                axis =
+                    Axis2d
+                        { originPoint = Point2d ( 2, 3 )
+                        , direction = Direction2d.y
+                        }
+
+                newOrigin =
+                    Point2d ( 4, 5 )
+            in
+                Axis2d.moveTo newOrigin axis
+                    |> Expect.axis2d
+                        (Axis2d
+                            { originPoint = Point2d ( 4, 5 )
+                            , direction = Direction2d.y
+                            }
+                        )
+
+
+documentationExamples : Test
+documentationExamples =
+    Test.describe "Documentation examples are correct"
+        [ xExample
+        , yExample
+        , perpendicularToExamples
+        , originPointExample
+        , directionExample
+        , flipExample
+        , moveToExample
+        ]
+
+
 suite : Test
 suite =
     Test.describe "OpenSolid.Geometry.Axis2d"
         [ jsonRoundTrips
+        , documentationExamples
         ]
 
 
