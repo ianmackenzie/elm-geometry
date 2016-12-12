d89e4201080aa7de75ccb25816a329b0c2af7284
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2016-12-12 17:44:02 -0500

    Start adding Axis3d documentation examples

--- tests/Axis3d.elm
+++ tests/Axis3d.elm
@@ -14,6 +14,10 @@ module Axis3d exposing (suite)
 
 import Test exposing (Test)
 import Test.Runner.Html as HtmlRunner
+import OpenSolid.Geometry.Types exposing (..)
+import OpenSolid.Axis3d as Axis3d
+import OpenSolid.Point3d as Point3d
+import OpenSolid.Direction3d as Direction3d
 import OpenSolid.Geometry.Encode as Encode
 import OpenSolid.Geometry.Decode as Decode
 import OpenSolid.Geometry.Fuzz as Fuzz
@@ -26,10 +30,75 @@ jsonRoundTrips =
     Generic.jsonRoundTrips Fuzz.axis3d Encode.axis3d Decode.axis3d
 
 
+xExample : Test
+xExample =
+    Test.test "Axis3d.x example" <|
+        \() ->
+            Axis3d.x
+                |> Expect.axis3d
+                    (Axis3d
+                        { originPoint = Point3d.origin
+                        , direction = Direction3d.x
+                        }
+                    )
+
+
+yExample : Test
+yExample =
+    Test.test "Axis3d.y example" <|
+        \() ->
+            Axis3d.y
+                |> Expect.axis3d
+                    (Axis3d
+                        { originPoint = Point3d.origin
+                        , direction = Direction3d.y
+                        }
+                    )
+
+
+zExample : Test
+zExample =
+    Test.test "Axis3d.z example" <|
+        \() ->
+            Axis3d.z
+                |> Expect.axis3d
+                    (Axis3d
+                        { originPoint = Point3d.origin
+                        , direction = Direction3d.z
+                        }
+                    )
+
+
+originPointExample : Test
+originPointExample =
+    Test.test "Axis3d.originPoint example" <|
+        \() ->
+            Axis3d.originPoint Axis3d.x |> Expect.point3d Point3d.origin
+
+
+directionExample : Test
+directionExample =
+    Test.test "Axis3d.direction example" <|
+        \() ->
+            Axis3d.direction Axis3d.y |> Expect.direction3d Direction3d.y
+
+
+documentationExamples : Test
+documentationExamples =
+    Test.describe "Documentation examples are correct"
+        [ xExample
+        , yExample
+        , zExample
+        , originPointExample
+        , directionExample
+        ]
+
+
 suite : Test
 suite =
     Test.describe "OpenSolid.Geometry.Axis3d"
         [ jsonRoundTrips
+        , documentationExamples
         ]
 
 
