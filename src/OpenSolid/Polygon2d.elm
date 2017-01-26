01521ad9b9515fe034c1401d7fd2c223cafff031
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2017-01-26 01:22:06 -0500

    Switch Polygon2d examples to more explicit format

--- src/OpenSolid/Polygon2d.elm
+++ src/OpenSolid/Polygon2d.elm
@@ -84,12 +84,12 @@ import OpenSolid.Triangle2d as Triangle2d
 
 {-| Get the vertices of a polygon.
 
-    Polygon2d.vertices rectangle ==
-        [ Point2d ( 1, 1 )
-        , Point2d ( 3, 1 )
-        , Point2d ( 3, 2 )
-        , Point2d ( 1, 2 )
-        ]
+    Polygon2d.vertices rectangle
+    --> [ Point2d ( 1, 1 )
+    --> , Point2d ( 3, 1 )
+    --> , Point2d ( 3, 2 )
+    --> , Point2d ( 1, 2 )
+    --> ]
 -}
 vertices : Polygon2d -> List Point2d
 vertices (Polygon2d vertices_) =
@@ -99,12 +99,12 @@ vertices (Polygon2d vertices_) =
 {-| Get the edges of a polygon. This will include an edge from the last point
 back to the first point.
 
-    Polygon2d.edges rectangle ==
-        [ LineSegment2d ( Point2d ( 1, 1 ), Point2d ( 3, 1 ) )
-        , LineSegment2d ( Point2d ( 3, 1 ), Point2d ( 3, 2 ) )
-        , LineSegment2d ( Point2d ( 3, 2 ), Point2d ( 1, 2 ) )
-        , LineSegment2d ( Point2d ( 1, 2 ), Point2d ( 1, 1 ) )
-        ]
+    Polygon2d.edges rectangle
+    --> [ LineSegment2d ( Point2d ( 1, 1 ), Point2d ( 3, 1 ) )
+    --> , LineSegment2d ( Point2d ( 3, 1 ), Point2d ( 3, 2 ) )
+    --> , LineSegment2d ( Point2d ( 3, 2 ), Point2d ( 1, 2 ) )
+    --> , LineSegment2d ( Point2d ( 1, 2 ), Point2d ( 1, 1 ) )
+    --> ]
 -}
 edges : Polygon2d -> List LineSegment2d
 edges polygon =
@@ -120,8 +120,8 @@ edges polygon =
 
 {-| Get the perimeter of a polygon (the sum of the lengths of its edges).
 
-    Polygon2d.perimeter rectangle ==
-        6
+    Polygon2d.perimeter rectangle
+    --> 6
 -}
 perimeter : Polygon2d -> Float
 perimeter =
@@ -130,8 +130,8 @@ perimeter =
 
 {-| Get the area of a polygon. This value will never be negative.
 
-    Polygon2d.area rectangle ==
-        2
+    Polygon2d.area rectangle
+    --> 2
 -}
 area : Polygon2d -> Float
 area =
@@ -142,8 +142,8 @@ area =
 order considered to have positive area and polygons with vertices in
 counterclockwise order considered to have negative area.
 
-    Polygon2d.clockwiseArea rectangle ==
-        -6
+    Polygon2d.clockwiseArea rectangle
+    --> -6
 -}
 clockwiseArea : Polygon2d -> Float
 clockwiseArea polygon =
@@ -154,8 +154,8 @@ clockwiseArea polygon =
 counterclockwise order considered to have positive area and polygons with
 vertices in clockwise order considered to have negative area.
 
-    Polygon2d.counterclockwiseArea rectangle ==
-        6
+    Polygon2d.counterclockwiseArea rectangle
+    --> 6
 -}
 counterclockwiseArea : Polygon2d -> Float
 counterclockwiseArea polygon =
@@ -185,13 +185,13 @@ counterclockwiseArea polygon =
     point =
         Point2d ( 2, 1 )
 
-    Polygon2d.scaleAbout point 2 rectangle ==
-        Polygon2d
-            [ Point2d ( 0, 1 )
-            , Point2d ( 4, 1 )
-            , Point2d ( 4, 3 )
-            , Point2d ( 0, 3 )
-            ]
+    Polygon2d.scaleAbout point 2 rectangle
+    --> Polygon2d
+    -->     [ Point2d ( 0, 1 )
+    -->     , Point2d ( 4, 1 )
+    -->     , Point2d ( 4, 3 )
+    -->     , Point2d ( 0, 3 )
+    -->     ]
 -}
 scaleAbout : Point2d -> Float -> Polygon2d -> Polygon2d
 scaleAbout point scale =
@@ -201,13 +201,13 @@ scaleAbout point scale =
 {-| Rotate a polygon around the given center point counterclockwise by the given
 angle (in radians).
 
-    Polygon2d.rotateAround Point2d.origin (degrees 90) rectangle ==
-        Polygon2d
-            [ Point2d ( -1, 1 )
-            , Point2d ( -1, 3 )
-            , Point2d ( -2, 3 )
-            , Point2d ( -2, 1 )
-            ]
+    Polygon2d.rotateAround Point2d.origin (degrees 90) rectangle
+    --> Polygon2d
+    -->     [ Point2d ( -1, 1 )
+    -->     , Point2d ( -1, 3 )
+    -->     , Point2d ( -2, 3 )
+    -->     , Point2d ( -2, 1 )
+    -->     ]
 -}
 rotateAround : Point2d -> Float -> Polygon2d -> Polygon2d
 rotateAround point angle =
@@ -219,13 +219,13 @@ rotateAround point angle =
     displacement =
         Vector2d ( 2, 3 )
 
-    Polygon2d.translateBy displacement rectangle ==
-        Polygon2d
-            [ Point2d ( 3, 4 )
-            , Point2d ( 5, 4 )
-            , Point2d ( 5, 5 )
-            , Point2d ( 3, 5 )
-            ]
+    Polygon2d.translateBy displacement rectangle
+    --> Polygon2d
+    -->     [ Point2d ( 3, 4 )
+    -->     , Point2d ( 5, 4 )
+    -->     , Point2d ( 5, 5 )
+    -->     , Point2d ( 3, 5 )
+    -->     ]
 -}
 translateBy : Vector2d -> Polygon2d -> Polygon2d
 translateBy vector =
@@ -234,13 +234,13 @@ translateBy vector =
 
 {-| Mirror a polygon across the given axis.
 
-    Polygon2d.mirrorAcross Axis2d.x rectangle ==
-        Polygon2d
-            [ Point2d ( 1, -1 )
-            , Point2d ( 3, -1 )
-            , Point2d ( 3, -2 )
-            , Point2d ( 1, -2 )
-            ]
+    Polygon2d.mirrorAcross Axis2d.x rectangle
+    --> Polygon2d
+    -->     [ Point2d ( 1, -1 )
+    -->     , Point2d ( 3, -1 )
+    -->     , Point2d ( 3, -2 )
+    -->     , Point2d ( 1, -2 )
+    -->     ]
 
 Note that if a polygon's vertices were in counterclockwise order before
 mirroring, they will be in clockwise order afterward, and vice versa.
@@ -253,8 +253,11 @@ mirrorAcross axis =
 {-| Transform each vertex of a polygon by the given function. All other
 transformations can be defined in terms of `map`, for example
 
-    Polygon2d.mirrorAcross Axis2d.x rectangle ==
-        Polygon2d.map (Point2d.mirrorAcross Axis2d.x) rectangle
+    Polygon2d.mirrorAcross Axis2d.x polygon
+
+is equivalent to
+
+    Polygon2d.map (Point2d.mirrorAcross Axis2d.x) polygon
 -}
 map : (Point2d -> Point2d) -> Polygon2d -> Polygon2d
 map function =
@@ -267,13 +270,13 @@ in local coordinates relative to a given reference frame.
     localFrame =
         Frame2d.at (Point2d ( 1, 2 ))
 
-    Polygon2d.relativeTo localFrame rectangle ==
-        Polygon2d
-            [ Point2d ( 0, -1 )
-            , Point2d ( 2, -1 )
-            , Point2d ( 2, 0 )
-            , Point2d ( 0, 0 )
-            ]
+    Polygon2d.relativeTo localFrame rectangle
+    --> Polygon2d
+    -->     [ Point2d ( 0, -1 )
+    -->     , Point2d ( 2, -1 )
+    -->     , Point2d ( 2, 0 )
+    -->     , Point2d ( 0, 0 )
+    -->     ]
 -}
 relativeTo : Frame2d -> Polygon2d -> Polygon2d
 relativeTo frame =
@@ -287,13 +290,13 @@ coordinates.
     localFrame =
         Frame2d.at (Point2d ( 1, 2 ))
 
-    Polygon2d.placeIn localFrame rectangle ==
-        Polygon2d
-            [ Point2d ( 2, 3 )
-            , Point2d ( 4, 3 )
-            , Point2d ( 4, 4 )
-            , Point2d ( 2, 4 )
-            ]
+    Polygon2d.placeIn localFrame rectangle
+    --> Polygon2d
+    -->     [ Point2d ( 2, 3 )
+    -->     , Point2d ( 4, 3 )
+    -->     , Point2d ( 4, 4 )
+    -->     , Point2d ( 2, 4 )
+    -->     ]
 -}
 placeIn : Frame2d -> Polygon2d -> Polygon2d
 placeIn frame =
@@ -303,15 +306,15 @@ placeIn frame =
 {-| Get the minimal bounding box containing a given polygon. Returns `Nothing`
 if the polygon has no vertices.
 
-    Polygon2d.boundingBox rectangle ==
-        Just
-            (BoundingBox2d
-                { minX = 1
-                , maxX = 3
-                , minY = 1
-                , maxY = 2
-                }
-            )
+    Polygon2d.boundingBox rectangle
+    --> Just
+    -->     (BoundingBox2d
+    -->         { minX = 1
+    -->         , maxX = 3
+    -->         , minY = 1
+    -->         , maxY = 2
+    -->         }
+    -->     )
 -}
 boundingBox : Polygon2d -> Maybe BoundingBox2d
 boundingBox polygon =
