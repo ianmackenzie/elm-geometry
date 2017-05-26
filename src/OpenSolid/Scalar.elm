3ff9514ace53f13d6b74372165028e1aaa73d484
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2017-05-26 15:58:55 -0400

    Fix documentation example formatting

--- src/OpenSolid/Scalar.elm
+++ src/OpenSolid/Scalar.elm
@@ -9,11 +9,11 @@ module OpenSolid.Scalar exposing (equalWithin, interpolateFrom)
 
 {-| Check if two values are equal within a given tolerance.
 
-    Scalar.equalWithin 1e-6 1.9999 2.0001 ==
-        False
+    Scalar.equalWithin 1e-6 1.9999 2.0001
+    --> False
 
-    Scalar.equalWithin 1e-3 1.9999 2.0001 ==
-        True
+    Scalar.equalWithin 1e-3 1.9999 2.0001
+    --> True
 
 -}
 equalWithin : Float -> Float -> Float -> Bool
