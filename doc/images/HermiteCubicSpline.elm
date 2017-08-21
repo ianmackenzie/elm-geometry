5e680b88db0351114cc31fb3d1cff2b91d7c6e5b
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2017-08-20 21:44:42 -0400

    Update Hermite spline image source

--- doc/images/HermiteCubicSpline.elm
+++ doc/images/HermiteCubicSpline.elm
@@ -1,11 +1,13 @@
 module HermiteCubicSpline2d exposing (..)
 
-import OpenSolid.Geometry.Types exposing (..)
-import OpenSolid.CubicSpline2d as CubicSpline2d
+import Html exposing (Html)
+import OpenSolid.BoundingBox2d as BoundingBox2d exposing (BoundingBox2d)
+import OpenSolid.CubicSpline2d as CubicSpline2d exposing (CubicSpline2d)
+import OpenSolid.Point2d as Point2d exposing (Point2d)
 import OpenSolid.Svg as Svg
+import OpenSolid.Vector2d as Vector2d exposing (Vector2d)
 import Svg exposing (Svg)
 import Svg.Attributes as Attributes
-import Html exposing (Html)
 
 
 drawPoint : Point2d -> Svg Never
@@ -39,7 +41,7 @@ main : Html Never
 main =
     let
         bounds =
-            BoundingBox2d
+            BoundingBox2d.with
                 { minX = 30
                 , maxX = 220
                 , minY = 30
@@ -47,16 +49,16 @@ main =
                 }
 
         p1 =
-            Point2d ( 50, 100 )
+            Point2d.withCoordinates ( 50, 100 )
 
         p2 =
-            Point2d ( 150, 100 )
+            Point2d.withCoordinates ( 150, 100 )
 
         v1 =
-            Vector2d ( 75, 75 )
+            Vector2d.withComponents ( 75, 75 )
 
         v2 =
-            Vector2d ( 50, -50 )
+            Vector2d.withComponents ( 50, -50 )
 
         spline =
             CubicSpline2d.hermite ( p1, v1 ) ( p2, v2 )
@@ -70,4 +72,4 @@ main =
                 , drawPoint p2
                 ]
     in
-        Svg.render2d bounds svg
+    Svg.render2d bounds svg
