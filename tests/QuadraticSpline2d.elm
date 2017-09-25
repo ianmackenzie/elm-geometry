9b660700e05bda659862dcf8a0bbaa7f5746a4ba
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2017-09-25 12:26:29 -0400

    Add test to verify arc length is computed correctly
    Test against an analytical solution - which breaks down for degenerate (linear) splines, so also make a custom fuzzer that only creates proper curved splines

--- tests/QuadraticSpline2d.elm
+++ tests/QuadraticSpline2d.elm
@@ -5,13 +5,108 @@ module QuadraticSpline2d
         )
 
 import Expect
+import Fuzz exposing (Fuzzer)
 import Generic
 import OpenSolid.Geometry.Decode as Decode
 import OpenSolid.Geometry.Encode as Encode
 import OpenSolid.Geometry.Fuzz as Fuzz
 import OpenSolid.Point2d as Point2d
 import OpenSolid.QuadraticSpline2d as QuadraticSpline2d exposing (QuadraticSpline2d)
+import OpenSolid.Scalar as Scalar
 import Test exposing (Test)
+
+
+curvedSpline : Fuzzer QuadraticSpline2d
+curvedSpline =
+    Fuzz.map5
+        (\angle length interpolationParameter offset flipSide ->
+            let
+                x0 =
+                    -length / 2
+
+                x2 =
+                    length / 2
+
+                x1 =
+                    Scalar.interpolateFrom x0 x2 interpolationParameter
+
+                y1 =
+                    if flipSide then
+                        -offset
+                    else
+                        offset
+
+                p0 =
+                    Point2d.fromCoordinates ( x0, 0 )
+
+                p1 =
+                    Point2d.fromCoordinates ( x1, y1 )
+
+                p2 =
+                    Point2d.fromCoordinates ( x2, 0 )
+            in
+            QuadraticSpline2d.fromControlPoints ( p0, p1, p2 )
+                |> QuadraticSpline2d.rotateAround Point2d.origin angle
+        )
+        (Fuzz.floatRange 0 (2 * pi))
+        (Fuzz.floatRange 1 10)
+        (Fuzz.floatRange 0 1)
+        (Fuzz.floatRange 1 5)
+        Fuzz.bool
+
+
+analyticalLength : QuadraticSpline2d -> Float
+analyticalLength spline =
+    let
+        ( p0, p1, p2 ) =
+            QuadraticSpline2d.controlPoints spline
+
+        ( x0, y0 ) =
+            Point2d.coordinates p0
+
+        ( x1, y1 ) =
+            Point2d.coordinates p1
+
+        ( x2, y2 ) =
+            Point2d.coordinates p2
+
+        ax =
+            x0 - 2 * x1 + x2
+
+        ay =
+            y0 - 2 * y1 + y2
+
+        bx =
+            2 * x1 - 2 * x0
+
+        by =
+            2 * y1 - 2 * y0
+
+        a =
+            4 * (ax * ax + ay * ay)
+
+        b =
+            4 * (ax * bx + ay * by)
+
+        c =
+            bx * bx + by * by
+
+        s_abc =
+            2 * sqrt (a + b + c)
+
+        a_2 =
+            sqrt a
+
+        a_32 =
+            2 * a * a_2
+
+        c_2 =
+            2 * sqrt c
+
+        ba =
+            b / a_2
+    in
+    (a_32 * s_abc + a_2 * b * (s_abc - c_2) + (4 * c * a - b * b) * logBase e ((2 * a_2 + ba + s_abc) / (ba + c_2))) / (4 * a_32)
 
 
 exampleSpline : QuadraticSpline2d
@@ -77,6 +172,21 @@ parameterization =
                 QuadraticSpline2d.arcLength parameterization
                     |> QuadraticSpline2d.parameterValueAtLength parameterization
                     |> Expect.equal (Just 1)
+        , Test.fuzz curvedSpline "arc length matches analytical formula" <|
+            -- analyticalLength falls down for degenerate splines so just check
+            -- curved ones
+            \spline ->
+                let
+                    tolerance =
+                        0.001
+
+                    parameterization =
+                        QuadraticSpline2d.arcLengthParameterization tolerance
+                            spline
+                in
+                QuadraticSpline2d.arcLength parameterization
+                    |> Expect.within (Expect.Absolute tolerance)
+                        (analyticalLength spline)
         , Test.test "at s=5 the result is the end point of the line" <|
             \_ ->
                 let
