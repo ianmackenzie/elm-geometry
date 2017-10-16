b89980128a4f9fe260233ea4a47fc16562b816f0
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2017-10-15 21:57:15 -0400

    Expose test utilities
    Will be useful for testing CubicSpline2d (convert curved quadratic spline to cubic and then check analytical arc length)

--- tests/QuadraticSpline2d.elm
+++ tests/QuadraticSpline2d.elm
@@ -1,7 +1,9 @@
 module QuadraticSpline2d
     exposing
-        ( jsonRoundTrips
+        ( analyticalLength
+        , curvedSpline
         , derivativeMagnitudeBoundsWorks
+        , jsonRoundTrips
         , parameterization
         )
 
