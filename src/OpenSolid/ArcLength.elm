1e902f3adfdec441d6f370d7f19bc894f8d97987
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2017-10-29 12:17:26 -0400

    Switch to 8 arc length segments per leaf
    Fewer object allocations, gives 30-40% performance boost in benchmarks

--- src/OpenSolid/ArcLength.elm
+++ src/OpenSolid/ArcLength.elm
@@ -62,12 +62,25 @@ type SegmentTree
         , length2 : Float
         , length3 : Float
         , length4 : Float
+        , length5 : Float
+        , length6 : Float
+        , length7 : Float
+        , length8 : Float
         , param0 : Float
         , param1 : Float
         , param2 : Float
         , param3 : Float
         , param4 : Float
+        , param5 : Float
+        , param6 : Float
+        , param7 : Float
+        , param8 : Float
         }
+
+
+segmentsPerLeaf : Int
+segmentsPerLeaf =
+    8
 
 
 {-| Build an arc length parameterization for a particular curve, within a given
@@ -87,8 +100,11 @@ buildParameterization config =
                 let
                     numSegments =
                         maxSecondDerivativeMagnitude / (8 * tolerance)
+
+                    numLeaves =
+                        numSegments / toFloat segmentsPerLeaf
                 in
-                max 0 (ceiling (logBase 2 (numSegments / 4)))
+                max 0 (ceiling (logBase 2 numLeaves))
     in
     ArcLengthParameterization (buildTree derivativeMagnitude 0 0 1 height)
 
@@ -114,22 +130,34 @@ buildTree derivativeMagnitude lengthAtStart paramAtStart paramAtEnd height =
                 paramAtStart
 
             param1 =
-                paramAtStart + 0.25 * paramDelta
+                paramAtStart + 0.125 * paramDelta
 
             param2 =
-                paramAtStart + 0.5 * paramDelta
+                paramAtStart + 0.25 * paramDelta
 
             param3 =
-                paramAtEnd - 0.25 * paramDelta
+                paramAtStart + 0.375 * paramDelta
 
             param4 =
+                paramAtStart + 0.5 * paramDelta
+
+            param5 =
+                paramAtEnd - 0.375 * paramDelta
+
+            param6 =
+                paramAtEnd - 0.25 * paramDelta
+
+            param7 =
+                paramAtEnd - 0.125 * paramDelta
+
+            param8 =
                 paramAtEnd
 
             offset =
-                0.125 * paramDelta
+                0.0625 * paramDelta
 
             paramStep =
-                0.25 * paramDelta
+                0.125 * paramDelta
 
             length0 =
                 lengthAtStart
@@ -145,6 +173,18 @@ buildTree derivativeMagnitude lengthAtStart paramAtStart paramAtEnd height =
 
             length4 =
                 length3 + derivativeMagnitude (param3 + offset) * paramStep
+
+            length5 =
+                length4 + derivativeMagnitude (param4 + offset) * paramStep
+
+            length6 =
+                length5 + derivativeMagnitude (param5 + offset) * paramStep
+
+            length7 =
+                length6 + derivativeMagnitude (param6 + offset) * paramStep
+
+            length8 =
+                length7 + derivativeMagnitude (param7 + offset) * paramStep
         in
         Leaf
             { param0 = param0
@@ -152,11 +192,19 @@ buildTree derivativeMagnitude lengthAtStart paramAtStart paramAtEnd height =
             , param2 = param2
             , param3 = param3
             , param4 = param4
+            , param5 = param5
+            , param6 = param6
+            , param7 = param7
+            , param8 = param8
             , length0 = length0
             , length1 = length1
             , length2 = length2
             , length3 = length3
             , length4 = length4
+            , length5 = length5
+            , length6 = length6
+            , length7 = length7
+            , length8 = length8
             }
     else
         let
@@ -221,32 +269,66 @@ toParameterValue (ArcLengthParameterization tree) s =
 unsafeToParameterValue : SegmentTree -> Float -> Float
 unsafeToParameterValue tree s =
     case tree of
-        Leaf { length0, length1, length2, length3, length4, param0, param1, param2, param3, param4 } ->
-            if s <= length2 then
-                if s <= length1 then
+        Leaf { length0, length1, length2, length3, length4, length5, length6, length7, length8, param0, param1, param2, param3, param4, param5, param6, param7, param8 } ->
+            if s <= length4 then
+                if s <= length2 then
+                    if s <= length1 then
+                        -- 0 to 1
+                        let
+                            lengthFraction =
+                                (s - length0) / (length1 - length0)
+                        in
+                        Scalar.interpolateFrom param0 param1 lengthFraction
+                    else
+                        -- 1 to 2
+                        let
+                            lengthFraction =
+                                (s - length1) / (length2 - length1)
+                        in
+                        Scalar.interpolateFrom param1 param2 lengthFraction
+                else if s <= length3 then
+                    -- 2 to 3
                     let
                         lengthFraction =
-                            (s - length0) / (length1 - length0)
+                            (s - length2) / (length3 - length2)
                     in
-                    Scalar.interpolateFrom param0 param1 lengthFraction
+                    Scalar.interpolateFrom param2 param3 lengthFraction
                 else
+                    -- 3 to 4
                     let
                         lengthFraction =
-                            (s - length1) / (length2 - length1)
+                            (s - length3) / (length4 - length3)
                     in
-                    Scalar.interpolateFrom param1 param2 lengthFraction
-            else if s <= length3 then
+                    Scalar.interpolateFrom param3 param4 lengthFraction
+            else if s <= length6 then
+                if s <= length5 then
+                    -- 4 to 5
+                    let
+                        lengthFraction =
+                            (s - length4) / (length5 - length4)
+                    in
+                    Scalar.interpolateFrom param4 param5 lengthFraction
+                else
+                    -- 5 to 6
+                    let
+                        lengthFraction =
+                            (s - length5) / (length6 - length5)
+                    in
+                    Scalar.interpolateFrom param5 param6 lengthFraction
+            else if s <= length7 then
+                -- 6 to 7
                 let
                     lengthFraction =
-                        (s - length2) / (length3 - length2)
+                        (s - length6) / (length7 - length6)
                 in
-                Scalar.interpolateFrom param2 param3 lengthFraction
+                Scalar.interpolateFrom param6 param7 lengthFraction
             else
+                -- 7 to 8
                 let
                     lengthFraction =
-                        (s - length3) / (length4 - length3)
+                        (s - length7) / (length8 - length7)
                 in
-                Scalar.interpolateFrom param3 param4 lengthFraction
+                Scalar.interpolateFrom param7 param8 lengthFraction
 
         Node { leftBranch, rightBranch } ->
             if s < lengthAtStart rightBranch then
@@ -289,32 +371,66 @@ fromParameterValue (ArcLengthParameterization tree) t =
 unsafeFromParameterValue : SegmentTree -> Float -> Float
 unsafeFromParameterValue tree t =
     case tree of
-        Leaf { length0, length1, length2, length3, length4, param0, param1, param2, param3, param4 } ->
-            if t <= param2 then
-                if t <= param1 then
+        Leaf { length0, length1, length2, length3, length4, length5, length6, length7, length8, param0, param1, param2, param3, param4, param5, param6, param7, param8 } ->
+            if t <= param4 then
+                if t <= param2 then
+                    if t <= param1 then
+                        -- 0 to 1
+                        let
+                            paramFraction =
+                                (t - param0) / (param1 - param0)
+                        in
+                        Scalar.interpolateFrom length0 length1 paramFraction
+                    else
+                        -- 1 to 2
+                        let
+                            paramFraction =
+                                (t - param1) / (param2 - param1)
+                        in
+                        Scalar.interpolateFrom length1 length2 paramFraction
+                else if t <= param3 then
+                    -- 2 to 3
                     let
                         paramFraction =
-                            (t - param0) / (param1 - param0)
+                            (t - param2) / (param3 - param2)
                     in
-                    Scalar.interpolateFrom length0 length1 paramFraction
+                    Scalar.interpolateFrom length2 length3 paramFraction
                 else
+                    -- 3 to 4
                     let
                         paramFraction =
-                            (t - param1) / (param2 - param1)
+                            (t - param3) / (param4 - param3)
                     in
-                    Scalar.interpolateFrom length1 length2 paramFraction
-            else if t <= param3 then
+                    Scalar.interpolateFrom length3 length4 paramFraction
+            else if t <= param6 then
+                if t <= param5 then
+                    -- 4 to 5
+                    let
+                        paramFraction =
+                            (t - param4) / (param5 - param4)
+                    in
+                    Scalar.interpolateFrom length4 length5 paramFraction
+                else
+                    -- 5 to 6
+                    let
+                        paramFraction =
+                            (t - param5) / (param6 - param5)
+                    in
+                    Scalar.interpolateFrom length5 length6 paramFraction
+            else if t <= param7 then
+                -- 6 to 7
                 let
                     paramFraction =
-                        (t - param2) / (param3 - param2)
+                        (t - param6) / (param7 - param6)
                 in
-                Scalar.interpolateFrom length2 length3 paramFraction
+                Scalar.interpolateFrom length6 length7 paramFraction
             else
+                -- 7 to 8
                 let
                     paramFraction =
-                        (t - param3) / (param4 - param3)
+                        (t - param7) / (param8 - param7)
                 in
-                Scalar.interpolateFrom length3 length4 paramFraction
+                Scalar.interpolateFrom length7 length8 paramFraction
 
         Node { leftBranch, rightBranch } ->
             if t < paramAtStart rightBranch then
