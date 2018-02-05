30b9d84b2ede5e9fa033295b9feec9d683fccfa2
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2018-02-05 15:23:09 -0500

    Add simple test polygon

--- src/OpenSolid/Polygon2d/Monotone.elm
+++ src/OpenSolid/Polygon2d/Monotone.elm
@@ -2,13 +2,15 @@ module OpenSolid.Polygon2d.Monotone
     exposing
         ( init
         , polygons
+        , testPolygon
         )
 
 import Array.Hamt as Array exposing (Array)
 import Dict exposing (Dict)
-import OpenSolid.Geometry.Internal as Internal exposing (Polygon2d(..))
+import OpenSolid.Geometry.Internal as Internal
 import OpenSolid.LineSegment2d as LineSegment2d exposing (LineSegment2d)
 import OpenSolid.Point2d as Point2d exposing (Point2d)
+import OpenSolid.Polygon2d as Polygon2d exposing (Polygon2d)
 import OpenSolid.Polygon2d.EdgeSet as EdgeSet exposing (EdgeSet)
 import Set
 
@@ -159,7 +161,7 @@ removeDuplicates points accumulatedPoints =
 
 
 init : Polygon2d -> Loops
-init (Polygon2d { outerLoop, innerLoops }) =
+init (Internal.Polygon2d { outerLoop, innerLoops }) =
     let
         allLoops =
             (outerLoop :: innerLoops)
@@ -548,6 +550,23 @@ collectMonotoneLoops state =
             List.foldl processStartEdge ( Set.empty, [] ) allEdgeIndices
     in
     ( points, loops )
+
+
+testPolygon : Polygon2d
+testPolygon =
+    let
+        loop =
+            List.map Point2d.fromCoordinates
+
+        outerLoop =
+            loop [ ( 0, 0 ), ( 5, 0 ), ( 5, 3 ), ( 0, 3 ) ]
+
+        innerLoops =
+            [ loop [ ( 1, 2 ), ( 2, 2 ), ( 2, 1 ), ( 1, 1 ) ]
+            , loop [ ( 3, 2 ), ( 4, 1.5 ), ( 3, 1 ) ]
+            ]
+    in
+    Polygon2d.withHoles outerLoop innerLoops
 
 
 polygons : Polygon2d -> ( Array Point2d, List (Array Int) )
