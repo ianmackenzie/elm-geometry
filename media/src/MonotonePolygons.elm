623976b1cdf637f8c37128b39802e8cbb4057bf7
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2018-02-06 15:07:56 -0500

    Improve monotone polygons visual test
    - Add inner hole to polygon
    - Colour each monotone loop separately
    - Allow rotating polygon via a slider to see how monotone polygons change

--- media/src/MonotonePolygons.elm
+++ media/src/MonotonePolygons.elm
@@ -2,7 +2,9 @@ module MonotonePolygones exposing (..)
 
 import Array.Hamt as Array
 import Html exposing (Html)
+import Html.Attributes
 import Html.Events
+import Kintail.InputWidget as InputWidget
 import OpenSolid.BoundingBox2d as BoundingBox2d exposing (BoundingBox2d)
 import OpenSolid.Point2d as Point2d exposing (Point2d)
 import OpenSolid.Polygon2d as Polygon2d exposing (Polygon2d)
@@ -16,12 +18,14 @@ import Svg.Attributes
 
 type alias Model =
     { polygon : Polygon2d
+    , angleInDegrees : Float
     }
 
 
 type Msg
     = Click
     | NewPolygon Polygon2d
+    | SetAngleInDegrees Float
 
 
 renderBounds : BoundingBox2d
@@ -49,32 +53,60 @@ polygonGenerator =
         maxRadius =
             0.5 * min width height - 10
 
-        radiusGenerator =
-            Random.float minRadius maxRadius
+        midRadius =
+            (minRadius + maxRadius) / 2
+
+        innerRadiusGenerator =
+            Random.float minRadius (midRadius - 5)
+
+        outerRadiusGenerator =
+            Random.float (midRadius + 5) maxRadius
     in
     Random.int 3 32
         |> Random.andThen
             (\numPoints ->
-                Random.list numPoints radiusGenerator
+                Random.list numPoints
+                    (Random.pair innerRadiusGenerator outerRadiusGenerator)
                     |> Random.map
                         (List.indexedMap
-                            (\index radius ->
+                            (\index ( innerRadius, outerRadius ) ->
                                 let
                                     angle =
                                         turns 1
                                             * toFloat index
                                             / toFloat numPoints
 
-                                    radialVector =
+                                    innerRadialVector =
                                         Vector2d.fromPolarComponents
-                                            ( radius
+                                            ( innerRadius
                                             , angle
                                             )
+
+                                    outerRadialVector =
+                                        Vector2d.fromPolarComponents
+                                            ( outerRadius
+                                            , angle
+                                            )
+
+                                    innerPoint =
+                                        centerPoint
+                                            |> Point2d.translateBy
+                                                innerRadialVector
+
+                                    outerPoint =
+                                        centerPoint
+                                            |> Point2d.translateBy
+                                                outerRadialVector
                                 in
-                                centerPoint |> Point2d.translateBy radialVector
+                                ( innerPoint, outerPoint )
                             )
                         )
-                    |> Random.map Polygon2d.singleLoop
+                    |> Random.map List.unzip
+                    |> Random.map
+                        (\( innerLoop, outerLoop ) ->
+                            Polygon2d.withHoles outerLoop
+                                [ List.reverse innerLoop ]
+                        )
             )
 
 
@@ -85,7 +117,9 @@ generateNewPolygon =
 
 init : ( Model, Cmd Msg )
 init =
-    ( { polygon = Polygon2d.singleLoop [] }, generateNewPolygon )
+    ( { polygon = Polygon2d.singleLoop [], angleInDegrees = 0 }
+    , generateNewPolygon
+    )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -97,6 +131,9 @@ update message model =
         NewPolygon polygon ->
             ( { model | polygon = polygon }, Cmd.none )
 
+        SetAngleInDegrees angleInDegrees ->
+            ( { model | angleInDegrees = angleInDegrees }, Cmd.none )
+
 
 
 --( Array Point2d, List (Array Int) )
@@ -105,29 +142,58 @@ update message model =
 view : Model -> Html Msg
 view model =
     let
-        ( vertices, loops ) =
-            Monotone.polygons model.polygon
+        ( width, height ) =
+            BoundingBox2d.dimensions renderBounds
 
-        drawLoop vertexIndices =
+        rotatedPolygon =
+            Polygon2d.rotateAround (BoundingBox2d.centroid renderBounds)
+                (degrees model.angleInDegrees)
+                model.polygon
+
+        ( vertices, loops ) =
+            Monotone.polygons rotatedPolygon
+
+        numLoops =
+            List.length loops
+
+        drawLoop loopIndex vertexIndices =
+            let
+                hueString =
+                    toString (360 * toFloat loopIndex / toFloat numLoops)
+
+                fillColor =
+                    "hsla(" ++ hueString ++ ",50%, 50%,0.5)"
+
+                strokeColor =
+                    "hsl(" ++ hueString ++ ",50%, 25%)"
+            in
             vertexIndices
                 |> Array.toList
                 |> List.filterMap (\index -> Array.get index vertices)
                 |> Polygon2d.singleLoop
                 |> Svg.polygon2d
-                    [ Svg.Attributes.fill "rgba(0, 127, 0, 0.25)"
-                    , Svg.Attributes.stroke "rgb(0, 127, 0)"
+                    [ Svg.Attributes.fill fillColor
+                    , Svg.Attributes.stroke strokeColor
                     ]
     in
-    Html.div [ Html.Events.onClick Click ]
-        [ Svg.render2d renderBounds <|
-            Svg.g []
-                [ Svg.g [] (List.map drawLoop loops)
-                , Svg.polygon2d
-                    [ Svg.Attributes.fill "none"
-                    , Svg.Attributes.stroke "black"
+    Html.div []
+        [ Html.div [ Html.Events.onClick Click ]
+            [ Svg.render2d renderBounds <|
+                Svg.g []
+                    [ Svg.g [] (List.indexedMap drawLoop loops)
+                    , Svg.polygon2d
+                        [ Svg.Attributes.fill "none"
+                        , Svg.Attributes.stroke "black"
+                        ]
+                        rotatedPolygon
                     ]
-                    model.polygon
-                ]
+            ]
+        , InputWidget.slider
+            [ Html.Attributes.style [ ( "width", toString width ++ "px" ) ] ]
+            { min = -180, max = 180, step = 1 }
+            model.angleInDegrees
+            |> Html.map SetAngleInDegrees
+        , Html.div [] [ Html.text (toString numLoops ++ " loops") ]
         ]
 
 
