aee2a5091bc5ae671e725b48c78374b99058aa91
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2018-02-07 13:05:33 -0500

    Add 'plain' polygon triangulation example
    Only use publicly exposed functionality, don't show monotone polygons (just the final triangles)

---
+++ media/src/PolygonTriangulation.elm
@@ -0,0 +1,188 @@
+module PolygonTriangulation exposing (..)
+
+import Html exposing (Html)
+import Html.Attributes
+import Html.Events
+import Kintail.InputWidget as InputWidget
+import OpenSolid.BoundingBox2d as BoundingBox2d exposing (BoundingBox2d)
+import OpenSolid.Mesh as Mesh
+import OpenSolid.Point2d as Point2d exposing (Point2d)
+import OpenSolid.Polygon2d as Polygon2d exposing (Polygon2d)
+import OpenSolid.Svg as Svg
+import OpenSolid.Triangle2d as Triangle2d exposing (Triangle2d)
+import OpenSolid.Vector2d as Vector2d exposing (Vector2d)
+import Random exposing (Generator)
+import Svg
+import Svg.Attributes
+
+
+type alias Model =
+    { polygon : Polygon2d
+    , angleInDegrees : Float
+    }
+
+
+type Msg
+    = Click
+    | NewPolygon Polygon2d
+    | SetAngleInDegrees Float
+
+
+renderBounds : BoundingBox2d
+renderBounds =
+    BoundingBox2d.fromExtrema
+        { minX = 0
+        , maxX = 800
+        , minY = 0
+        , maxY = 600
+        }
+
+
+polygonGenerator : Generator Polygon2d
+polygonGenerator =
+    let
+        centerPoint =
+            BoundingBox2d.centroid renderBounds
+
+        ( width, height ) =
+            BoundingBox2d.dimensions renderBounds
+
+        minRadius =
+            10
+
+        maxRadius =
+            0.5 * min width height - 10
+
+        midRadius =
+            (minRadius + maxRadius) / 2
+
+        innerRadiusGenerator =
+            Random.float minRadius (midRadius - 5)
+
+        outerRadiusGenerator =
+            Random.float (midRadius + 5) maxRadius
+    in
+    Random.int 3 32
+        |> Random.andThen
+            (\numPoints ->
+                Random.list numPoints
+                    (Random.pair innerRadiusGenerator outerRadiusGenerator)
+                    |> Random.map
+                        (List.indexedMap
+                            (\index ( innerRadius, outerRadius ) ->
+                                let
+                                    angle =
+                                        turns 1
+                                            * toFloat index
+                                            / toFloat numPoints
+
+                                    innerRadialVector =
+                                        Vector2d.fromPolarComponents
+                                            ( innerRadius
+                                            , angle
+                                            )
+
+                                    outerRadialVector =
+                                        Vector2d.fromPolarComponents
+                                            ( outerRadius
+                                            , angle
+                                            )
+
+                                    innerPoint =
+                                        centerPoint
+                                            |> Point2d.translateBy
+                                                innerRadialVector
+
+                                    outerPoint =
+                                        centerPoint
+                                            |> Point2d.translateBy
+                                                outerRadialVector
+                                in
+                                ( innerPoint, outerPoint )
+                            )
+                        )
+                    |> Random.map List.unzip
+                    |> Random.map
+                        (\( innerLoop, outerLoop ) ->
+                            Polygon2d.withHoles outerLoop
+                                [ List.reverse innerLoop ]
+                        )
+            )
+
+
+generateNewPolygon : Cmd Msg
+generateNewPolygon =
+    Random.generate NewPolygon polygonGenerator
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( { polygon = Polygon2d.singleLoop [], angleInDegrees = 0 }
+    , generateNewPolygon
+    )
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update message model =
+    case message of
+        Click ->
+            ( model, generateNewPolygon )
+
+        NewPolygon polygon ->
+            ( { model | polygon = polygon }, Cmd.none )
+
+        SetAngleInDegrees angleInDegrees ->
+            ( { model | angleInDegrees = angleInDegrees }, Cmd.none )
+
+
+view : Model -> Html Msg
+view model =
+    let
+        ( width, height ) =
+            BoundingBox2d.dimensions renderBounds
+
+        rotatedPolygon =
+            Polygon2d.rotateAround (BoundingBox2d.centroid renderBounds)
+                (degrees model.angleInDegrees)
+                model.polygon
+
+        mesh =
+            Polygon2d.triangulate rotatedPolygon
+
+        triangles =
+            Mesh.faces mesh |> List.map Triangle2d.fromVertices
+
+        drawTriangle =
+            Svg.triangle2d
+                [ Svg.Attributes.fill "rgba(127, 127, 127, 0.25)"
+                , Svg.Attributes.stroke "rgb(127, 127, 127)"
+                ]
+    in
+    Html.div []
+        [ Html.div [ Html.Events.onClick Click ]
+            [ Svg.render2d renderBounds <|
+                Svg.g []
+                    [ Svg.g [] (List.map drawTriangle triangles)
+                    , Svg.polygon2d
+                        [ Svg.Attributes.fill "none"
+                        , Svg.Attributes.stroke "black"
+                        ]
+                        rotatedPolygon
+                    ]
+            ]
+        , InputWidget.slider
+            [ Html.Attributes.style [ ( "width", toString width ++ "px" ) ] ]
+            { min = -180, max = 180, step = 1 }
+            model.angleInDegrees
+            |> Html.map SetAngleInDegrees
+        ]
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = always Sub.none
+        }
