4446333559f92d1d102194302c476dcfbf3c8dc5
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2018-04-23 00:04:38 -0400

    Document SweptAngle module

--- src/Geometry/SweptAngle.elm
+++ src/Geometry/SweptAngle.elm
@@ -7,7 +7,17 @@ module Geometry.SweptAngle
         , smallPositive
         )
 
-{-|
+{-| When constructing circular or elliptical arcs, it is sometimes necessary to
+specify which of several possible arcs you want. For example, if you ask for a
+circular arc from the point (1, 0) to the point (0, 1) with a radius of 1, there
+are four possible solutions:
+
+  - An arc with a swept angle of 90 degrees, with center point at (0, 0)
+  - An arc with a swept angle of -270 degrees, with center point at (0, 0)
+  - An arc with a swept angle of -90 degrees, with center point at (1, 1)
+  - An Arc with a swept angle of 270 degrees, with center point at (1, 1)
+
+The `SweptAngle` type is used in these cases to specify which arc you want.
 
 @docs SweptAngle, smallPositive, smallNegative, largePositive, largeNegative
 
@@ -16,25 +26,38 @@ module Geometry.SweptAngle
 import Geometry.Internal as Internal
 
 
+{-| Indicate which of four possible arcs you would like to construct. Used by
+[`Arc2d.withRadius`](Arc2d#withRadius) and [`EllipticalArc2d.fromEndpoints`](EllipticalArc2d#fromEndpoints).
+-}
 type alias SweptAngle =
     Internal.SweptAngle
 
 
+{-| Construct a counterclockwise arc with a swept angle between 0 and 180
+degrees.
+-}
 smallPositive : SweptAngle
 smallPositive =
     Internal.SmallPositive
 
 
+{-| Construct a clockwise arc with a swept angle between 0 and -180 degrees.
+-}
 smallNegative : SweptAngle
 smallNegative =
     Internal.SmallNegative
 
 
+{-| Construct a counterclockwise arc with a swept angle between 180 and 360
+degrees.
+-}
 largePositive : SweptAngle
 largePositive =
     Internal.LargePositive
 
 
+{-| Construct a clockwise arc with a swept angle between -180 and -360 degrees.
+-}
 largeNegative : SweptAngle
 largeNegative =
     Internal.LargeNegative
