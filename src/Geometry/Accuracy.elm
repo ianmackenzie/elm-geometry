c42b865445bb0989e30069c5ff7cb846d08904fa
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2018-04-23 12:24:42 -0400

    Document Accuracy module

--- src/Geometry/Accuracy.elm
+++ src/Geometry/Accuracy.elm
@@ -1,12 +1,31 @@
 module Geometry.Accuracy exposing (Accuracy, maxError)
 
+{-| The `Accuracy` type is used to control the accuracy of approximations such
+as `Arc2d.toPolyline` and `CubicSpline3d.arcLengthParameterized`.
+
+@docs Accuracy, maxError
+
+-}
+
 import Geometry.Internal as Internal
 
 
+{-| Opaque type used to specify the accuracy of some approximation. Currently
+the only supported way to specify accuracy is 'maximum error', but in the future
+other criteria such as 'number of approximating segments' may be added.
+-}
 type alias Accuracy =
     Internal.Accuracy
 
 
+{-| Specify the maximum error for a given approximation. For example, calling
+
+    Arc2d.toPolyline (Accuracy.maxError 0.001) arc
+
+will ensure that the distance between the returned polyline and the actual arc
+will never be greater than 0.001 units.
+
+-}
 maxError : Float -> Accuracy
 maxError =
     Internal.MaxError
