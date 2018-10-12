028ebd70626bfce606de664193eebc03e514282d
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2018-10-12 17:11:15 -0400

    Add Quantity.[aXbY,aXbYcZ]
    Useful for linear equations such as matrix multiplications

--- src/Quantity/Extra.elm
+++ src/Quantity/Extra.elm
@@ -1,4 +1,4 @@
-module Quantity.Extra exposing (interpolateFrom)
+module Quantity.Extra exposing (aXbY, aXbYcZ, interpolateFrom)
 
 import Float.Extra as Float
 import Quantity exposing (Quantity(..))
@@ -7,3 +7,13 @@ import Quantity exposing (Quantity(..))
 interpolateFrom : Quantity Float units -> Quantity Float units -> Float -> Quantity Float units
 interpolateFrom (Quantity start) (Quantity end) parameter =
     Quantity (Float.interpolateFrom start end parameter)
+
+
+aXbY : Float -> Quantity Float units -> Float -> Quantity Float units -> Quantity Float units
+aXbY a (Quantity x) b (Quantity y) =
+    Quantity (a * x + b * y)
+
+
+aXbYcZ : Float -> Quantity Float units -> Float -> Quantity Float units -> Float -> Quantity Float units -> Quantity Float units
+aXbYcZ a (Quantity x) b (Quantity y) c (Quantity z) =
+    Quantity (a * x + b * y + c * z)
