cc1204c10984f55336b221c7df5b069e4568f64b
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2018-11-22 21:19:43 -0500

    Update Triangle2d to use units/coordinates

--- src/Triangle2d.elm
+++ src/Triangle2d.elm
@@ -54,6 +54,7 @@ Transforming a triangle is equivalent to transforming its vertices.
 
 -}
 
+import Angle exposing (Angle)
 import Axis2d exposing (Axis2d)
 import BoundingBox2d exposing (BoundingBox2d)
 import Circle2d exposing (Circle2d)
@@ -62,12 +63,14 @@ import Frame2d exposing (Frame2d)
 import Geometry.Types as Types
 import LineSegment2d exposing (LineSegment2d)
 import Point2d exposing (Point2d)
+import Quantity exposing (Quantity, Squared)
+import Quantity.Extra as Quantity
 import Vector2d exposing (Vector2d)
 
 
 {-| -}
-type alias Triangle2d =
-    Types.Triangle2d
+type alias Triangle2d units coordinates =
+    Types.Triangle2d units coordinates
 
 
 {-| Construct a triangle from its three vertices:
@@ -80,7 +83,7 @@ type alias Triangle2d =
             )
 
 -}
-fromVertices : ( Point2d, Point2d, Point2d ) -> Triangle2d
+fromVertices : ( Point2d units coordinates, Point2d units coordinates, Point2d units coordinates ) -> Triangle2d units coordinates
 fromVertices =
     Types.Triangle2d
 
@@ -97,7 +100,7 @@ fromVertices =
     --> p3 = Point2d.fromCoordinates ( 1, 3 )
 
 -}
-vertices : Triangle2d -> ( Point2d, Point2d, Point2d )
+vertices : Triangle2d units coordinates -> ( Point2d units coordinates, Point2d units coordinates, Point2d units coordinates )
 vertices (Types.Triangle2d vertices_) =
     vertices_
 
@@ -129,7 +132,7 @@ second to the third, and from the third back to the first.
     -->         )
 
 -}
-edges : Triangle2d -> ( LineSegment2d, LineSegment2d, LineSegment2d )
+edges : Triangle2d units coordinates -> ( LineSegment2d units coordinates, LineSegment2d units coordinates, LineSegment2d units coordinates )
 edges triangle =
     let
         ( p1, p2, p3 ) =
@@ -147,7 +150,7 @@ edges triangle =
     --> Point2d.fromCoordinates ( 1.3333, 1.6667 )
 
 -}
-centroid : Triangle2d -> Point2d
+centroid : Triangle2d units coordinates -> Point2d units coordinates
 centroid triangle =
     let
         ( p1, p2, p3 ) =
@@ -160,7 +163,8 @@ centroid triangle =
             Vector2d.from p1 p3
 
         displacement =
-            Vector2d.scaleBy (1.0 / 3.0) (Vector2d.sum firstVector secondVector)
+            Vector2d.scaleBy (1.0 / 3.0)
+                (firstVector |> Vector2d.plus secondVector)
     in
     Point2d.translateBy displacement p1
 
@@ -180,7 +184,7 @@ It does not matter whether the triangle's vertices are in clockwise or
 counterclockwise order.
 
 -}
-contains : Point2d -> Triangle2d -> Bool
+contains : Point2d units coordinates -> Triangle2d units coordinates -> Bool
 contains point triangle =
     let
         ( p1, p2, p3 ) =
@@ -205,8 +209,14 @@ contains point triangle =
         thirdProduct =
             crossProduct p3 p1
     in
-    (firstProduct >= 0 && secondProduct >= 0 && thirdProduct >= 0)
-        || (firstProduct <= 0 && secondProduct <= 0 && thirdProduct <= 0)
+    ((firstProduct |> Quantity.greaterThanOrEqualTo Quantity.zero)
+        && (secondProduct |> Quantity.greaterThanOrEqualTo Quantity.zero)
+        && (thirdProduct |> Quantity.greaterThanOrEqualTo Quantity.zero)
+    )
+        || ((firstProduct |> Quantity.lessThanOrEqualTo Quantity.zero)
+                && (secondProduct |> Quantity.lessThanOrEqualTo Quantity.zero)
+                && (thirdProduct |> Quantity.lessThanOrEqualTo Quantity.zero)
+           )
 
 
 {-| Get the area of a triangle. The result will always be positive regardless of
@@ -216,9 +226,9 @@ whether the triangle's vertices are in clockwise or counterclockwise order.
     --> 1.0
 
 -}
-area : Triangle2d -> Float
+area : Triangle2d units coordinates -> Quantity Float (Squared units)
 area =
-    counterclockwiseArea >> abs
+    counterclockwiseArea >> Quantity.abs
 
 
 {-| Get the signed area of a triangle, returning a positive value if the
@@ -229,7 +239,7 @@ otherwise.
     --> 1.0
 
 -}
-counterclockwiseArea : Triangle2d -> Float
+counterclockwiseArea : Triangle2d units coordinates -> Quantity Float (Squared units)
 counterclockwiseArea triangle =
     let
         ( p1, p2, p3 ) =
@@ -241,7 +251,7 @@ counterclockwiseArea triangle =
         secondVector =
             Vector2d.from p1 p3
     in
-    0.5 * Vector2d.crossProduct firstVector secondVector
+    Quantity.scaleBy 0.5 (Vector2d.crossProduct firstVector secondVector)
 
 
 {-| Get the signed area of a triangle, returning a positive value if the
@@ -251,9 +261,9 @@ triangle's vertices are in clockwise order and a negative value otherwise.
     --> -1.0
 
 -}
-clockwiseArea : Triangle2d -> Float
+clockwiseArea : Triangle2d units coordinates -> Quantity Float (Squared units)
 clockwiseArea triangle =
-    -(counterclockwiseArea triangle)
+    Quantity.negate (counterclockwiseArea triangle)
 
 
 {-| Scale a triangle about a given point by a given scale.
@@ -271,7 +281,7 @@ order before the negative scaling, they will be in clockwise order afterwards
 and vice versa.
 
 -}
-scaleAbout : Point2d -> Float -> Triangle2d -> Triangle2d
+scaleAbout : Point2d units coordinates -> Float -> Triangle2d units coordinates -> Triangle2d units coordinates
 scaleAbout point scale =
     mapVertices (Point2d.scaleAbout point scale)
 
@@ -288,7 +298,7 @@ scaleAbout point scale =
     -->     )
 
 -}
-rotateAround : Point2d -> Float -> Triangle2d -> Triangle2d
+rotateAround : Point2d units coordinates -> Angle -> Triangle2d units coordinates -> Triangle2d units coordinates
 rotateAround centerPoint angle =
     mapVertices (Point2d.rotateAround centerPoint angle)
 
@@ -306,7 +316,7 @@ rotateAround centerPoint angle =
     -->     )
 
 -}
-translateBy : Vector2d -> Triangle2d -> Triangle2d
+translateBy : Vector2d units coordinates -> Triangle2d units coordinates -> Triangle2d units coordinates
 translateBy vector =
     mapVertices (Point2d.translateBy vector)
 
@@ -321,7 +331,7 @@ is equivalent to
         (Vector2d.withLength distance direction)
 
 -}
-translateIn : Direction2d -> Float -> Triangle2d -> Triangle2d
+translateIn : Direction2d coordinates -> Quantity Float units -> Triangle2d units coordinates -> Triangle2d units coordinates
 translateIn direction distance triangle =
     translateBy (Vector2d.withLength distance direction) triangle
 
@@ -340,7 +350,7 @@ flipped - if the triangle's vertices were in counterclockwise order before
 mirroring, they will be in clockwise order afterwards and vice versa.
 
 -}
-mirrorAcross : Axis2d -> Triangle2d -> Triangle2d
+mirrorAcross : Axis2d units coordinates -> Triangle2d units coordinates -> Triangle2d units coordinates
 mirrorAcross axis =
     mapVertices (Point2d.mirrorAcross axis)
 
@@ -356,7 +366,7 @@ is equivalent to
     Triangle2d.mapVertices (Point2d.mirrorAcross axis)
 
 -}
-mapVertices : (Point2d -> Point2d) -> Triangle2d -> Triangle2d
+mapVertices : (Point2d unitsA coordinatesA -> Point2d unitsB coordinatesB) -> Triangle2d unitsA coordinatesA -> Triangle2d unitsB coordinatesB
 mapVertices function triangle =
     let
         ( p1, p2, p3 ) =
@@ -379,7 +389,7 @@ in local coordinates relative to a given reference frame.
     -->     )
 
 -}
-relativeTo : Frame2d -> Triangle2d -> Triangle2d
+relativeTo : Frame2d units globalCoordinates { defines : localCoordinates } -> Triangle2d units globalCoordinates -> Triangle2d units localCoordinates
 relativeTo frame =
     mapVertices (Point2d.relativeTo frame)
 
@@ -398,7 +408,7 @@ given reference frame, and return that triangle expressed in global coordinates.
     -->     )
 
 -}
-placeIn : Frame2d -> Triangle2d -> Triangle2d
+placeIn : Frame2d units globalCoordinates { defines : localCoordinates } -> Triangle2d units localCoordinates -> Triangle2d units globalCoordinates
 placeIn frame =
     mapVertices (Point2d.placeIn frame)
 
@@ -414,7 +424,7 @@ placeIn frame =
     -->     }
 
 -}
-boundingBox : Triangle2d -> BoundingBox2d
+boundingBox : Triangle2d units coordinates -> BoundingBox2d units coordinates
 boundingBox triangle =
     let
         ( p1, p2, p3 ) =
@@ -430,10 +440,10 @@ boundingBox triangle =
             Point2d.coordinates p3
     in
     BoundingBox2d.fromExtrema
-        { minX = min x1 (min x2 x3)
-        , maxX = max x1 (max x2 x3)
-        , minY = min y1 (min y2 y3)
-        , maxY = max y1 (max y2 y3)
+        { minX = Quantity.min x1 (Quantity.min x2 x3)
+        , maxX = Quantity.max x1 (Quantity.max x2 x3)
+        , minY = Quantity.min y1 (Quantity.min y2 y3)
+        , maxY = Quantity.max y1 (Quantity.max y2 y3)
         }
 
 
@@ -453,7 +463,7 @@ If the triangle is degenerate (its three vertices are collinear), returns
 `Nothing`.
 
 -}
-circumcircle : Triangle2d -> Maybe Circle2d
+circumcircle : Triangle2d units coordinates -> Maybe (Circle2d units coordinates)
 circumcircle triangle =
     let
         ( p1, p2, p3 ) =
