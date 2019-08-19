40f71b0e03f9923cd963b874c780a18c16501594
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2019-08-19 19:00:44 -0400

    Fix example formatting
    Avoid horizontal scroll bars on package web site

--- src/Polyline3d.elm
+++ src/Polyline3d.elm
@@ -368,7 +368,8 @@ boundingBox polyline =
 polyline has no vertices.
 
     Polyline3d.centroid examplePolyline
-    --> Just (Point3d.fromCoordinates ( 0.9167, 1.333, 0.75 ))
+    --> Just <|
+    -->     Point3d.fromCoordinates ( 0.9167, 1.333, 0.75 )
 
 -}
 centroid : Polyline3d -> Maybe Point3d
