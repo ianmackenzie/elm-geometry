45b1556c830b1df475d3e2b4a0b8cb30715bb145
Guilherme Belmonte <contact@belmonte.dev>
2020-10-10 01:07:32 -0300

    Add vector 3d tests

--- tests/Tests/Vector3d.elm
+++ tests/Tests/Vector3d.elm
@@ -1,8 +1,9 @@
-module Tests.Vector3d exposing (sum)
+module Tests.Vector3d exposing (sum, vectorScaling)
 
 import Fuzz
 import Geometry.Expect as Expect
 import Geometry.Fuzz as Fuzz
+import Quantity
 import Test exposing (Test)
 import Vector3d
 
@@ -14,3 +15,19 @@ sum =
             Vector3d.sum vectors
                 |> Expect.vector3d
                     (List.foldl Vector3d.plus Vector3d.zero vectors)
+
+
+vectorScaling : Test
+vectorScaling =
+    Test.describe "scaling 3d vectors"
+        [ Test.fuzz (Fuzz.tuple ( Fuzz.length, Fuzz.vector3d )) "scaleTo has a consistent length" <|
+            \( scale, vector ) ->
+                Vector3d.scaleTo scale vector
+                    |> Vector3d.length
+                    |> Expect.quantity (Quantity.abs scale)
+        , Test.fuzz Fuzz.vector3d "normalize has a consistent length" <|
+            \vector ->
+                Vector3d.normalize vector
+                    |> Vector3d.length
+                    |> Expect.quantity (Quantity.float 1)
+        ]
