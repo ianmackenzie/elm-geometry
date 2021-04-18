59e89a2b6124ef0795487acb81b2ff8703d7155c
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2021-04-18 13:32:57 -0400

    Add several more random generators for tests

--- tests/Geometry/Random.elm
+++ tests/Geometry/Random.elm
@@ -1,9 +1,17 @@
 module Geometry.Random exposing
     ( angle
+    , arc2d
+    , arc3d
     , cubicSpline2d
     , cubicSpline3d
+    , direction2d
+    , direction3d
+    , ellipse2d
+    , ellipticalArc2d
+    , ellipticalArc3d
     , length
     , parameterValue
+    , plane3d
     , positiveLength
     , quadraticSpline2d
     , quadraticSpline3d
@@ -12,6 +20,7 @@ module Geometry.Random exposing
     , rationalQuadraticSpline2d
     , rationalQuadraticSpline3d
     , scale
+    , sketchPlane3d
     , unitlessInterval
     , unitlessQuantity
     , vector2d
@@ -23,9 +32,17 @@ module Geometry.Random exposing
     )
 
 import Angle exposing (Angle)
+import Arc2d exposing (Arc2d)
+import Arc3d exposing (Arc3d)
 import CubicSpline2d exposing (CubicSpline2d)
 import CubicSpline3d exposing (CubicSpline3d)
+import Direction2d exposing (Direction2d)
+import Direction3d exposing (Direction3d)
+import Ellipse2d exposing (Ellipse2d)
+import EllipticalArc2d exposing (EllipticalArc2d)
+import EllipticalArc3d exposing (EllipticalArc3d)
 import Length exposing (Length, Meters)
+import Plane3d exposing (Plane3d)
 import Point2d exposing (Point2d)
 import Point3d exposing (Point3d)
 import QuadraticSpline2d exposing (QuadraticSpline2d)
@@ -37,6 +54,7 @@ import RationalCubicSpline2d exposing (RationalCubicSpline2d)
 import RationalCubicSpline3d exposing (RationalCubicSpline3d)
 import RationalQuadraticSpline2d exposing (RationalQuadraticSpline2d)
 import RationalQuadraticSpline3d exposing (RationalQuadraticSpline3d)
+import SketchPlane3d exposing (SketchPlane3d)
 import Vector2d exposing (Vector2d)
 import Vector3d exposing (Vector3d)
 import Vector4d exposing (Vector4d)
@@ -86,6 +104,34 @@ positiveLength =
 angle : Generator Angle
 angle =
     Random.map Angle.radians (Random.float (-2 * pi) (2 * pi))
+
+
+direction2d : Generator (Direction2d coordinates)
+direction2d =
+    Random.map Direction2d.fromAngle angle
+
+
+direction3d : Generator (Direction3d coordinates)
+direction3d =
+    let
+        phiGenerator =
+            Random.map (acos >> Angle.radians) (Random.float -1 1)
+
+        thetaGenerator =
+            Random.map Angle.radians (Random.float -pi pi)
+
+        toDirection phi theta =
+            let
+                r =
+                    Angle.sin phi
+            in
+            Direction3d.unsafe
+                { x = r * Angle.cos theta
+                , y = r * Angle.sin theta
+                , z = Angle.cos phi
+                }
+    in
+    Random.map2 toDirection phiGenerator thetaGenerator
 
 
 vector2d : Generator (Vector2d Meters coordinates)
@@ -190,3 +236,75 @@ rationalCubicSpline3d =
 cubicSpline3d : Generator (CubicSpline3d Meters coordinates)
 cubicSpline3d =
     Random.map4 CubicSpline3d.fromControlPoints point3d point3d point3d point3d
+
+
+plane3d : Generator (Plane3d Meters coordinates)
+plane3d =
+    Random.map2 Plane3d.through point3d direction3d
+
+
+sketchPlane3d : Generator (SketchPlane3d Meters coordinates { defines : sketchCoordinates })
+sketchPlane3d =
+    let
+        sketchPlane plane rotationAngle =
+            SketchPlane3d.fromPlane plane
+                |> SketchPlane3d.rotateAroundOwn SketchPlane3d.normalAxis rotationAngle
+    in
+    Random.map2 sketchPlane plane3d angle
+
+
+arc2d : Generator (Arc2d Meters coordinates)
+arc2d =
+    Random.map3 Arc2d.from
+        point2d
+        point2d
+        (Random.uniform
+            (Random.float -350 350)
+            [ Random.float 370 710
+            , Random.float -710 -370
+            ]
+            |> Random.andThen (Random.map Angle.degrees)
+        )
+
+
+arc3d : Generator (Arc3d Meters coordinates)
+arc3d =
+    Random.map2 Arc3d.on sketchPlane3d arc2d
+
+
+ellipse2d : Generator (Ellipse2d Meters coordinates)
+ellipse2d =
+    let
+        ellipse centerPoint xDirection xRadius yRadius =
+            Ellipse2d.with
+                { centerPoint = centerPoint
+                , xDirection = xDirection
+                , xRadius = xRadius
+                , yRadius = yRadius
+                }
+    in
+    Random.map4 ellipse point2d direction2d positiveLength positiveLength
+
+
+ellipticalArc2d : Generator (EllipticalArc2d Meters coordinates)
+ellipticalArc2d =
+    let
+        ellipticalArc ( centerPoint, xDirection ) ( xRadius, yRadius ) ( startAngle, sweptAngle ) =
+            EllipticalArc2d.with
+                { centerPoint = centerPoint
+                , xDirection = xDirection
+                , xRadius = xRadius
+                , yRadius = yRadius
+                , startAngle = startAngle
+                , sweptAngle = sweptAngle
+                }
+    in
+    Random.map3 ellipticalArc
+        (Random.map2 Tuple.pair point2d direction2d)
+        (Random.map2 Tuple.pair positiveLength positiveLength)
+        (Random.map2 Tuple.pair angle angle)
+
+
+ellipticalArc3d : Generator (EllipticalArc3d Meters coordinates)
+ellipticalArc3d =
+    Random.map2 EllipticalArc3d.on sketchPlane3d ellipticalArc2d
